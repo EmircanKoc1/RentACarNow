23427574cde7f8cda08980f1e798e295344f280d
Emircan <emircandv787@gmail.com>
2024-07-11 01:30:41 +0300

    azuthorize extension was defined

---
+++ src/APIs/ReadAPI/Presentation/RentACarNow.APIs.ReadAPI.WebAPI/Extensions/AuthorizeExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using RentACarNow.Common.Constants.JWT;
+using RentACarNow.Common.Infrastructure.Extensions;
+
+namespace RentACarNow.APIs.ReadAPI.WebAPI.Extensions
+{
+    public static class AuthorizeExtensions
+    {
+
+        public static IServiceCollection SetAuthorize(this IServiceCollection services, bool active)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                      .AddJwtBearer(options =>
+                      {
+                          options.TokenValidationParameters = new TokenValidationParameters
+                          {
+                              ValidateIssuer = true,
+                              ValidateAudience = true,
+                              ValidateLifetime = true,
+                              ValidateIssuerSigningKey = true,
+
+                              IssuerSigningKey = new SymmetricSecurityKey(JWTConstants.SECURITY_KEY.ConvertToByteArray()),
+                              ValidAudience = JWTConstants.AUDIENCE,
+                              ValidIssuer = JWTConstants.ISSUER,
+                          };
+
+
+                      });
+
+
+            services.AddAuthorization(
+                config =>
+                {
+                    config.AddPolicy("ReadAPI.Brand", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Brand");
+                    });
+
+                    config.AddPolicy("ReadAPI.Admin", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Admin");
+                    });
+
+                    config.AddPolicy("ReadAPI.Customer", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Customer");
+                    });
+
+                    config.AddPolicy("ReadAPI.Car", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Car");
+                    });
+
+                    config.AddPolicy("ReadAPI.Claim", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Claim");
+                    });
+
+                    config.AddPolicy("ReadAPI.Employee", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Employee");
+                    });
+
+                    config.AddPolicy("ReadAPI.Feature", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Feature");
+                    });
+
+                    config.AddPolicy("ReadAPI.Rental", policy =>
+                    {
+                        if (active)
+                            policy.RequireClaim("Permission", "ReadAPI.Rental");
+                    });
+
+                });
+
+            return services;
+        }
+
+    }
+}
