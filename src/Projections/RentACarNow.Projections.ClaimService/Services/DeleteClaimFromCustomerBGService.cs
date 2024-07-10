6a084c1f9a7c08a9bd04bcd0f63d8460514d8daa
Emircan <emircandv787@gmail.com>
2024-07-10 03:01:55 +0300

    claim delete from routing keys and queues were defined.

---
+++ src/Projections/RentACarNow.Projections.ClaimService/Services/DeleteClaimFromCustomerBGService.cs
@@ -0,0 +1,33 @@
+﻿using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+
+namespace RentACarNow.Projections.ClaimService.Services
+{
+    public class DeleteClaimFromCustomerBGService : BackgroundService
+    {
+        private readonly IMongoClaimWriteRepository _writeRepository;
+        private readonly ILogger<DeleteClaimFromCustomerBGService> _logger;
+        private readonly IRabbitMQMessageService _messageService;
+
+        public DeleteClaimFromCustomerBGService(IMongoClaimWriteRepository writeRepository, ILogger<DeleteClaimFromCustomerBGService> logger, IRabbitMQMessageService messageService)
+        {
+            _writeRepository = writeRepository;
+            _logger = logger;
+            _messageService = messageService;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+            _messageService.ConsumeQueue();
+
+
+
+
+            return Task.CompletedTask;
+
+
+
+        }
+    }
+}
