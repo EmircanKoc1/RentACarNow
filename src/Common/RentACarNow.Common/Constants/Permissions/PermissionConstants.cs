9aacce48354b314b05a518034dd0fc132e8b2b47
Emircan <emircandv787@gmail.com>
2024-08-03 23:36:36 +0300

    permisson classes constants were defined and class  was created

---
+++ src/Common/RentACarNow.Common/Constants/Permissions/PermissionConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Constants.Permissions
+{
+    public static class PermissionConstants
+    {
+        public const string USER_DEFAULT_PERMISSION = "User";
+        public const string EMPLOYEE_DEFAULT_PERMISSION = "Employee";
+        public const string ADMIN_DEFAULT_PERMISSION = "Admin";
+
+    }
+}
+
