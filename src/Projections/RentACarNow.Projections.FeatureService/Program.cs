32f487df0a580b136e23a2910ddedc7ba4165a19
Emircan <emircandv787@gmail.com>
2024-08-03 23:33:22 +0300

    new extension methods were added programcs

--- src/Projections/RentACarNow.Projections.FeatureService/Program.cs
+++ src/Projections/RentACarNow.Projections.FeatureService/Program.cs
@@ -1,6 +1,7 @@
 using RabbitMQ.Client;
 using RentACarNow.Common.Constants.Databases;
 using RentACarNow.Common.Constants.MessageBrokers.UriAndUrl;
+using RentACarNow.Common.Infrastructure.Extensions;
 using RentACarNow.Common.Infrastructure.Repositories.Implementations.Write.Mongo;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
 using RentACarNow.Common.Infrastructure.Services.Implementations;
@@ -19,21 +20,24 @@ namespace RentACarNow.Projections.FeatureService
 
             builder.Services.AddSingleton<IMongoFeatureWriteRepository, MongoFeatureWriteRepository>();
 
-            builder.Services.AddSingleton<IRabbitMQMessageService, RabbitMQMessageService>(x =>
-            {
-                return new RabbitMQMessageService(new ConnectionFactory()
-                {
-                    Uri = new Uri(RabbitMQUrisAndUrls.RABBITMQ_URI)
-                });
-            });
+            //builder.Services.AddSingleton<IRabbitMQMessageService, RabbitMQMessageService>(x =>
+            //{
+            //    return new RabbitMQMessageService(new ConnectionFactory()
+            //    {
+            //        Uri = new Uri(RabbitMQUrisAndUrls.RABBITMQ_URI)
+            //    });
+            //});
 
-            builder.Services.AddSingleton<MongoRentalACarNowDbContext>(x =>
-            {
-                return new MongoRentalACarNowDbContext(
-                    connectionString: MongoDbConstants.CONNECTION_STRING,
-                    databaseName: MongoDbConstants.DATABASE_NAME);
-            });
+            //builder.Services.AddSingleton<MongoRentalACarNowDbContext>(x =>
+            //{
+            //    return new MongoRentalACarNowDbContext(
+            //        connectionString: MongoDbConstants.CONNECTION_STRING,
+            //        databaseName: MongoDbConstants.DATABASE_NAME);
+            //});
 
+            builder.Services.AddIRabbitMQMessageService(clientName: "FeatureService");
+
+            builder.Services.AddMongoRentalACarNowDBContext();
 
             builder.Services.AddHostedService<FeatureAddedBGService>();
             builder.Services.AddHostedService<FeatureDeletedBGService>();
