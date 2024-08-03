aace71046f7d356bb6ecdf93e3bd8e21347a6ca7
Emircan <emircandv787@gmail.com>
2024-08-03 23:36:03 +0300

    added addmongorentalacarnowdbcontext extension method

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/ServiceRegistrations.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/ServiceRegistrations.cs
@@ -3,7 +3,7 @@ using RentACarNow.Common.Constants.Databases;
 using RentACarNow.Common.Infrastructure.Repositories.Implementations.Read.Mongo;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
 using RentACarNow.Common.MongoContexts.Implementations;
-
+using RentACarNow.Common.Infrastructure.Extensions;
 namespace RentACarNow.APIs.ReadAPI.Application
 {
     public static class ServiceRegistrations
@@ -19,8 +19,6 @@ namespace RentACarNow.APIs.ReadAPI.Application
 
             services.AddAutoMapper(assembly);
 
-
-
             services.AddSingleton<IMongoAdminReadRepository, MongoAdminReadRepository>();
             services.AddSingleton<IMongoBrandReadRepository, MongoBrandReadRepository>();
             services.AddSingleton<IMongoCustomerReadRepository, MongoCustomerReadRepository>();
@@ -30,15 +28,18 @@ namespace RentACarNow.APIs.ReadAPI.Application
             services.AddSingleton<IMongoEmployeeReadRepository, MongoEmployeeReadRepository>();
             services.AddSingleton<IMongoRentalReadRepository, MongoRentalReadRepository>();
 
-            services.AddSingleton<MongoRentalACarNowDbContext>(x =>
-            {
-                return new MongoRentalACarNowDbContext(
-                    connectionString: MongoDbConstants.CONNECTION_STRING,
-                    databaseName: MongoDbConstants.DATABASE_NAME);
-            });
+            //services.AddSingleton<MongoRentalACarNowDbContext>(x =>
+            //{
+            //    return new MongoRentalACarNowDbContext(
+            //        connectionString: MongoDbConstants.CONNECTION_STRING,
+            //        databaseName: MongoDbConstants.DATABASE_NAME);
+            //});
+
+            services.AddMongoRentalACarNowDBContext();
 
             return services;
 
+          
 
         }
     }
