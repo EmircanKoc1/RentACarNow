1f14dd3f3596a2768cd1cce99af79472e8b4b30d
Emircan <emircandv787@gmail.com>
2024-08-15 18:10:52 +0300

    The common event classes have been defined in the RentACarNow.Common.Events class library

---
+++ src/Common/RentACarNow.Common.Events/Common/BaseEvent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+
+namespace RentACarNow.Common.Events.Common
+{
+    //public record BaseEvent(DateTime? CreatedDate, DateTime? UpdatedDate, DateTime? DeletedDate) : IEvent;
+
+    public class BaseEvent : IEvent
+    {
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+    }
+}
