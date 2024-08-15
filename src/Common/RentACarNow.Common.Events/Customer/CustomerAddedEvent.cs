4d978e0d1c86da6ea2bae58da39f2866aae37e01
Emircan <emircandv787@gmail.com>
2024-08-15 18:11:06 +0300

    The Customer event classes have been defined in the RentACarNow.Common.Events class library

---
+++ src/Common/RentACarNow.Common.Events/Customer/CustomerAddedEvent.cs
@@ -0,0 +1,27 @@
+﻿using RentACarNow.Common.Events.Common;
+using RentACarNow.Common.Events.Common.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Events.Customer
+{
+    public class CustomerAddedEvent : BaseEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal WalletBalance { get; set; }
+
+        public ICollection<ClaimMessage> Claims { get; set; }
+
+
+    }
+}
---
+++ src/Common/RentACarNow.Common.Events/Customer/CustomerDeletedEvent.cs
@@ -0,0 +1,9 @@
+﻿using RentACarNow.Common.Events.Common;
+
+namespace RentACarNow.Common.Events.Customer
+{
+    public class CustomerDeletedEvent : BaseEvent
+    {
+        public Guid Id { get; set; }
+    }
+}
