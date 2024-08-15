8c89a1a12b4e4784fde79f7ff35dea3b6119841d
Emircan <emircandv787@gmail.com>
2024-08-15 04:21:45 +0300

    The RegisterUserCommand classes have been defined.

---
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Auth/Register/RegisterUserCommandRequest.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.Runtime.Internal;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Auth.Register
+{
+    public class RegisterUserCommandRequest : IRequest<RegisterUserCommandResponse>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+   
+
+    }
+}
---
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Auth/Register/RegisterUserCommandRequestHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Auth.Register
+{
+    public class RegisterUserCommandRequestHandler : IRequestHandler<RegisterUserCommandRequest, RegisterUserCommandResponse>
+    {
+        public Task<RegisterUserCommandResponse> Handle(RegisterUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
