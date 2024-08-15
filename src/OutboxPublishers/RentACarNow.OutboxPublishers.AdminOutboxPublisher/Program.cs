f29fdcc080b57e84e7a89c68d08698f9837705c8
Emircan <emircandv787@gmail.com>
2024-08-15 16:21:47 +0300

    The AdminOutboxPublisher workser service has been deleted to the project

--- src/OutboxPublishers/RentACarNow.OutboxPublishers.AdminOutboxPublisher/Program.cs
+++
@@ -1,7 +0,0 @@
-using RentACarNow.OutboxPublishers.AdminOutboxPublisher;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
