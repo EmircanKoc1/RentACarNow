fec93cdbd2b23991e980f78da750609c8208317e
Emircan <emircandv787@gmail.com>
2024-08-15 17:47:30 +0300

    The FilterParameter,OrderingParameter , and PaginationParameter classes have been defined  in the  RentACarNow.Common.Models class library

---
+++ src/Common/RentACarNow.Common.Models/OrderingParameter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Models
+{
+    public class OrderingParameter
+    {
+        public bool Sort { get; set; } 
+        public bool IsAscending { get; set; } = true;
+        public string SortingField { get; set; }
+    }
+}
---
+++ src/Common/RentACarNow.Common.Models/PaginationParameter.cs
@@ -0,0 +1,45 @@
+﻿namespace RentACarNow.Common.Models
+{
+    public class PaginationParameter
+    {
+        public PaginationParameter(int pageNumber, int size)
+        {
+            PageNumber = pageNumber;
+            Size = size;
+        }
+
+        public PaginationParameter()
+        {
+
+        }
+
+        public static PaginationParameter CreatePaginationParameter(int pageNumber, int size)
+            => new PaginationParameter(pageNumber, size);
+
+        private readonly int maxPageSize = 100;
+        private int size = 1;
+        private int pageNumber = 1;
+
+        public int PageNumber
+        {
+            get => pageNumber;
+            set => pageNumber = value <= 0 ? 1 : value;
+
+        }
+        public int Size
+        {
+            get => size;
+            set
+            {
+                if (value <= 0)
+                    size = 1;
+                else if (value > maxPageSize)
+                    size = maxPageSize;
+                else
+                    size = value;
+            }
+        }
+
+
+    }
+}
