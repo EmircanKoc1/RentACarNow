c6d44c701555df5ce33b7fc694c82f4ff7a2db27
Emircan <emircandv787@gmail.com>
2024-08-15 20:35:45 +0300

    The Events.txt has been  created .

---
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Domain/Entities/EfCoreEntities/User.cs
@@ -0,0 +1,21 @@
+﻿using RentACarNow.APIs.WriteAPI.Domain.Entities.Common.Concrete;
+using RentACarNow.APIs.WriteAPI.Domain.Entities.Common.Interfaces;
+
+namespace RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities
+{
+    public class User : EFBaseEntity, IEFEntity
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public decimal WalletBalance { get; set; }
+
+        public ICollection<Claim> Claims { get; set; } = new HashSet<Claim>();
+    }
+}
