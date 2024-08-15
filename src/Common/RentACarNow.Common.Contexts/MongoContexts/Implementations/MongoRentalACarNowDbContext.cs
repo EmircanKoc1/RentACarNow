06f7505540bb70c2530f386ae855f1535a0e3d2d
Emircan <emircandv787@gmail.com>
2024-08-15 18:18:28 +0300

    The MongoContexts have been defined in the RentACar.Common.Contexts class library

---
+++ src/Common/RentACarNow.Common.Contexts/MongoContexts/Implementations/MongoRentalACarNowDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using RentACarNow.Common.MongoContexts.Interfaces;
+using RentACarNow.Common.MongoEntities;
+using RentACarNow.Common.MongoEntities.Common.Interfaces;
+using RentACarNow.Common.Settings;
+
+namespace RentACarNow.Common.MongoContexts.Implementations
+{
+    public class MongoRentalACarNowDbContext : IMongoDBContext
+    {
+        protected readonly IMongoDatabase _database;
+
+        public MongoRentalACarNowDbContext(IConfiguration configuration)
+        {
+            IMongoClient client = new MongoClient(configuration["MongoDb:ConnectionString"]);
+            _database = client.GetDatabase(configuration["MongoDb:DatabaseName"]);
+        }
+        public MongoRentalACarNowDbContext(MongoClient client, string dbName)
+        {
+            IMongoClient _client = client;
+            _database = _client.GetDatabase(dbName);
+        }
+
+        public MongoRentalACarNowDbContext(string connectionString, string databaseName)
+        {
+            IMongoClient _client = new MongoClient(connectionString: connectionString);
+            _database = _client.GetDatabase(databaseName);
+
+        }
+
+        public MongoRentalACarNowDbContext(IOptions<MongoDbSettings> settings)
+        {
+            if (settings is null)
+                throw new Exception();
+
+            IMongoClient client = new MongoClient(connectionString: settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.DatabaseName);
+
+        }
+
+        public IMongoCollection<Admin> AdminCollection => GetCollection<Admin>();
+        public IMongoCollection<Customer> CustomerCollection => GetCollection<Customer>();
+        public IMongoCollection<Car> CarCollection => GetCollection<Car>();
+        public IMongoCollection<Brand> BrandCollection => GetCollection<Brand>();
+        public IMongoCollection<Rental> RentalCollection => GetCollection<Rental>();
+        public IMongoCollection<Employee> EmployeeCollection => GetCollection<Employee>();
+
+        public IMongoCollection<T> GetCollection<T>() where T : IMongoEntity
+            => _database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+
+
+    }
+}
---
+++ src/Common/RentACarNow.Common.Contexts/MongoContexts/Interfaces/IMongoDBContext.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using RentACarNow.Common.MongoEntities;
+using RentACarNow.Common.MongoEntities.Common.Interfaces;
+
+namespace RentACarNow.Common.MongoContexts.Interfaces
+{
+    public interface IMongoDBContext
+    {
+        IMongoCollection<Admin> AdminCollection { get; }
+        IMongoCollection<Customer> CustomerCollection { get; }
+        IMongoCollection<Car> CarCollection { get; }
+        IMongoCollection<Brand> BrandCollection { get; }
+        IMongoCollection<Rental> RentalCollection { get; }
+        IMongoCollection<Employee> EmployeeCollection { get; }
+        IMongoCollection<T> GetCollection<T>() where T : IMongoEntity;
+
+    }
+}
