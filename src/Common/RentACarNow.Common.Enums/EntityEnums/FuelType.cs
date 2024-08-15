91b85b40ce6b0496b65288a5c6af7e257767c3b6
Emircan <emircandv787@gmail.com>
2024-08-15 17:51:04 +0300

    The EmployeePosition , FuelType , RentalStatus , TransmissionType  and WorkEnvirenment classes have been defined in the RentACarNow.Common.Enums class library

---
+++ src/Common/RentACarNow.Common.Enums/EntityEnums/FuelType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Enums.EntityEnums
+{
+    public enum FuelType
+    {
+        Electric,
+        Gasoline,
+        Diesel,
+        Hybird,
+        CNG,
+        LPG,
+        FlexFuel
+
+    }
+}
---
+++ src/Common/RentACarNow.Common.Enums/EntityEnums/RentalStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Enums.EntityEnums
+{
+    public enum RentalStatus
+    {
+        Preparing,
+        Active,
+        Completed,
+        Cancelled
+
+    }
+}
