f3418b1c1e82043c1ae7668f288961e4235cb9f1
Emircan <emircandv787@gmail.com>
2024-08-15 18:07:52 +0300

    The Car event classes have been defined in the RentACarNow.Common.Events class library

---
+++ src/Common/RentACarNow.Common.Events/Car/CarAddedEvent.cs
@@ -0,0 +1,26 @@
+﻿using RentACarNow.Common.Enums.EntityEnums;
+using RentACarNow.Common.Events.Common;
+using RentACarNow.Common.Events.Common.Messages;
+
+namespace RentACarNow.Common.Events.Car
+{
+    public class CarAddedEvent : BaseEvent , IEvent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Modal { get; set; }
+        public string Title { get; set; }
+        public decimal HourlyRentalPrice { get; set; }
+        public float Kilometer { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public int PassengerCapacity { get; set; }
+        public int LuggageCapacity { get; set; }
+        public float FuelConsumption { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public FuelType CarFuelType { get; set; }
+        public TransmissionType TransmissionType { get; set; }
+        public BrandMessage Brand { get; set; }
+        public ICollection<FeatureMessage> Features { get; set; }
+    }
+}
