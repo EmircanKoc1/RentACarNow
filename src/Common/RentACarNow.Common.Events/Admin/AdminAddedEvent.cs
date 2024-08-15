7325b47fdd3b8558db2197d1ace887207c4cbef2
Emircan <emircandv787@gmail.com>
2024-08-15 18:06:48 +0300

    The Admin event classes have been defined .

---
+++ src/Common/RentACarNow.Common.Events/Admin/AdminAddedEvent.cs
@@ -0,0 +1,20 @@
+﻿using RentACarNow.Common.Events.Common;
+using RentACarNow.Common.Events.Common.Messages;
+
+namespace RentACarNow.Common.Events.Admin
+{
+    public class AdminAddedEvent : BaseEvent
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<ClaimMessage> Claims { get; set; }
+
+    }
+
+
+
+
+}
---
+++ src/Common/RentACarNow.Common.Events/Admin/AdminDeletedEvent.cs
@@ -0,0 +1,11 @@
+﻿using RentACarNow.Common.Events.Common;
+
+namespace RentACarNow.Common.Events.Admin
+{
+    public class AdminDeletedEvent : BaseEvent
+    {
+        public Guid Id { get; set; }
+    }
+
+
+}
