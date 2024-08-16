8454167a0bf7d7ce4953d8a2911ceb94e1cf212f
Emircan <emircandv787@gmail.com>
2024-08-16 04:21:42 +0300

    The MongoBrandOutbox, MongoUserOutbox, MongoClaimOutbox classes, and the BaseMongoOutboxContext abstract class have implemented interfaces.

---
+++ src/Common/RentACarNow.Common.Contexts/OutboxContexts/MongoContexts/Implementations/BaseMongoOutboxContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Interfaces;
+using RentACarNow.Common.Entities.OutboxEntities;
+
+namespace RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations
+{
+    public abstract class BaseMongoOutboxContext<TEntity> : IBaseMongoOutboxContext<TEntity>
+        where TEntity : IMongoOutbox
+    {
+        private readonly IMongoDatabase _mongoDatabase;
+        protected BaseMongoOutboxContext(MongoClient mongoClient, string databaseName)
+        {
+            IMongoClient client = mongoClient;
+            _mongoDatabase = client.GetDatabase(databaseName);
+        }
+
+
+        public IMongoCollection<TEntity> GetCollection => _mongoDatabase.GetCollection<TEntity>(nameof(TEntity).ToLowerInvariant());
+
+
+
+
+
+    }
+}
