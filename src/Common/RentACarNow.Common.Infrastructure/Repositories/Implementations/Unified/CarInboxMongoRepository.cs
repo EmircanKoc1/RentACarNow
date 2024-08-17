653a951991bc8d4ffeeb82e9c0a176b1fad256d0
Emircan <emircandv787@gmail.com>
2024-08-17 23:35:31 +0300

    Reapply "The BaseMongoInboxContext has been modified ."
    This reverts commit fb5b0d891ea41668fd35ed9c2dd06b12670c2674.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Implementations/Unified/CarInboxMongoRepository.cs
@@ -0,0 +1,16 @@
+﻿using RentACarNow.Common.Contexts.InboxContexts.Implementations;
+using RentACarNow.Common.Entities.InboxEntities;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Base;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+
+namespace RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified
+{
+    public class CarInboxMongoRepository : BaseInboxMongoRepository<CarInboxMessage, MongoCarInboxContext>, ICarInboxRepository
+    {
+        public CarInboxMongoRepository(MongoCarInboxContext context) : base(context)
+        {
+        }
+    }
+
+
+}
