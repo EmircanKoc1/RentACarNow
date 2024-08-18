4100354208bfb52f689665e2779fc6c41c2a9303
Emircan <emircandv787@gmail.com>
2024-08-18 21:21:18 +0300

    The IEFDBContext has been refactored .

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Contexts/IEfDBContext.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Contexts/IEfDBContext.cs
@@ -6,16 +6,17 @@ namespace RentACarNow.APIs.WriteAPI.Application.Contexts
 {
     public interface IEfDBContext
     {
-        DbSet<Admin> Admins { get; set; }
+        //DbSet<Admin> Admins { get; set; }
+        //DbSet<Customer> Customers { get; set; }
+        //DbSet<Employee> Employees { get; set; }
+        DbSet<User> Users { get; set; } 
+
         DbSet<Brand> Brands { get; set; }
         DbSet<Car> Cars { get; set; }
-        DbSet<Customer> Customers { get; set; }
-        DbSet<Employee> Employees { get; set; }
         DbSet<Feature> Features { get; set; }
         DbSet<Rental> Rentals { get; set; }
         DbSet<Claim> Claims { get; set; }
 
-        DbSet<User> Users { get; set; } 
 
     }
 }
