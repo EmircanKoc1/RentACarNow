fe93e99ec17b2aa0ab2cb866c4b1e285e688e287
Emircan <emircandv787@gmail.com>
2024-08-26 21:08:00 +0300

    The Admin,Employee,Customer repository interfaces and classes have been deleted .

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Repositories/Write/EfCore/IEfCoreAdminWriteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using RentACarNow.APIs.WriteAPI.Application.Repositories.Base;
-using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
-
-namespace RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore
-{
-    public interface IEfCoreAdminWriteRepository : IEfWriteRepository<Admin>
-    {
-    }
-}
