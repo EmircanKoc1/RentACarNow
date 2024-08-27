06a330d67d88eeaf258f7e1e69b8b17eb7f56ae2
Emircan <emircandv787@gmail.com>
2024-08-27 04:01:09 +0300

    The UpdateClaimcommand and DeleteClaimCommand handlers have been refactored .

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/DeleteClaim/DeleteClaimCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/DeleteClaim/DeleteClaimCommandRequestHandler.cs
@@ -4,10 +4,11 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
-using RentACarNow.Common.Constants.MessageBrokers.Exchanges;
-using RentACarNow.Common.Constants.MessageBrokers.RoutingKeys;
+using RentACarNow.Common.Entities.OutboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.Claim;
-using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteClaim
@@ -16,6 +17,7 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteCl
     {
         private readonly IEfCoreClaimWriteRepository _writeRepository;
         private readonly IEfCoreClaimReadRepository _readRepository;
+        private readonly IClaimOutboxRepository _claimOutboxRepository;
         private readonly IValidator<DeleteClaimCommandRequest> _validator;
         private readonly ILogger<DeleteClaimCommandRequestHandler> _logger;
         private readonly IMapper _mapper;
@@ -25,13 +27,15 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteCl
             IEfCoreClaimReadRepository readRepository,
             IValidator<DeleteClaimCommandRequest> validator,
             ILogger<DeleteClaimCommandRequestHandler> logger,
-            IMapper mapper)
+            IMapper mapper,
+            IClaimOutboxRepository claimOutboxRepository)
         {
             _writeRepository = writeRepository;
             _readRepository = readRepository;
             _validator = validator;
             _logger = logger;
             _mapper = mapper;
+            _claimOutboxRepository = claimOutboxRepository;
         }
 
         public async Task<DeleteClaimCommandResponse> Handle(DeleteClaimCommandRequest request, CancellationToken cancellationToken)
@@ -52,12 +56,42 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteCl
 
             var claimEntity = _mapper.Map<EfEntity.Claim>(request);
 
-            _writeRepository.Delete(claimEntity);
-            _writeRepository.SaveChanges();
-
             var claimDeletedEvent = _mapper.Map<ClaimDeletedEvent>(claimEntity);
 
-       
+            using var efTran = _writeRepository.BeginTransaction();
+            using var mongoSession = await _claimOutboxRepository.StartSessionAsync();
+
+            try
+            {
+                mongoSession.StartTransaction();
+
+
+                _writeRepository.Delete(claimEntity);
+                _writeRepository.SaveChanges();
+
+                await _claimOutboxRepository.AddMessageAsync(new ClaimOutboxMessage
+                {
+                    AddedDate = DateTime.UtcNow,
+                    ClaimEventType = ClaimEventType.ClaimDeletedEvent,
+                    Id = Guid.NewGuid(),
+                    Payload = claimDeletedEvent.Serialize()!
+
+                }, mongoSession);
+
+
+
+
+                efTran.Commit();
+                await mongoSession.CommitTransactionAsync();
+            }
+            catch (Exception)
+            {
+                await mongoSession.AbortTransactionAsync();
+                efTran.Rollback();
+                throw;
+            }
+
+
             return new DeleteClaimCommandResponse();
         }
     }
--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/UpdateClaim/UpdateClaimCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/UpdateClaim/UpdateClaimCommandRequestHandler.cs
@@ -4,7 +4,10 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.Claim;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateClaim
@@ -13,6 +16,7 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
     {
         private readonly IEfCoreClaimWriteRepository _writeRepository;
         private readonly IEfCoreClaimReadRepository _readRepository;
+        private readonly IClaimOutboxRepository _outboxRepository;
         private readonly IValidator<UpdateClaimCommandRequest> _validator;
         private readonly ILogger<UpdateClaimCommandRequestHandler> _logger;
         private readonly IMapper _mapper;
@@ -21,13 +25,15 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
             IEfCoreClaimReadRepository readRepository,
             IValidator<UpdateClaimCommandRequest> validator,
             ILogger<UpdateClaimCommandRequestHandler> logger,
-            IMapper mapper)
+            IMapper mapper,
+            IClaimOutboxRepository outboxRepository)
         {
             _writeRepository = writeRepository;
             _readRepository = readRepository;
             _validator = validator;
             _logger = logger;
             _mapper = mapper;
+            _outboxRepository = outboxRepository;
         }
 
         public async Task<UpdateClaimCommandResponse> Handle(UpdateClaimCommandRequest request, CancellationToken cancellationToken)
@@ -47,11 +53,41 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
 
             var claimEntity = _mapper.Map<EfEntity.Claim>(request);
 
-
-            await _writeRepository.UpdateAsync(claimEntity);
-            await _writeRepository.SaveChangesAsync();
-
             var claimUpdatedEvent = _mapper.Map<ClaimUpdatedEvent>(claimEntity);
+
+            using var efTran = await _writeRepository.BeginTransactionAsync();
+            using var mongoSession = await _outboxRepository.StartSessionAsync();
+
+
+            try
+            {
+                mongoSession.StartTransaction();
+
+                await _writeRepository.UpdateAsync(claimEntity);
+                await _writeRepository.SaveChangesAsync();
+
+
+                await _outboxRepository.AddMessageAsync(new Common.Entities.OutboxEntities.ClaimOutboxMessage
+                {
+                    AddedDate = DateTime.Now,
+                    ClaimEventType = ClaimEventType.ClaimUpdatedEvent,
+                    Id = Guid.NewGuid(),
+                    Payload = claimUpdatedEvent.Serialize()!
+
+
+                }, mongoSession);
+
+                await mongoSession.CommitTransactionAsync();
+                await efTran.CommitAsync();
+
+            }
+            catch (Exception)
+            {
+                await efTran.RollbackAsync();
+                await mongoSession.AbortTransactionAsync();
+
+                throw;
+            }
 
 
             return new UpdateClaimCommandResponse { };
