ec79de03c5309920b449e5140eba42b5b5898f15
Emircan <emircandv787@gmail.com>
2024-08-28 14:37:33 +0300

    The UserMapProfile has been created and its content filled in.

---
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Mapping/UserMapProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.CreateUser;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.DeleteUser;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUser;
+using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
+using RentACarNow.Common.Events.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Mapping
+{
+    public class UserMapProfile : Profile
+    {
+        public UserMapProfile()
+        {
+
+            CreateMap<User, UserCreatedEvent>();
+            CreateMap<User, UserDeletedEvent>();
+            CreateMap<User, UserUpdatedEvent>();
+            
+
+
+            CreateMap<CreateUserCommandRequest, User>();
+            CreateMap<UpdateUserCommandRequest, User>();
+            CreateMap<DeleteUserCommandRequest, User>();
+
+        }
+
+    }
+}
