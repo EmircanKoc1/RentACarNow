ee2d62ca64272d91d8d737127f1cdae371c6f969
Emircan <emircandv787@gmail.com>
2024-08-28 12:17:42 +0300

    The FeatureMapProfile class has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Mapping/FeatureMapProfile.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Mapping/FeatureMapProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using RentACarNow.APIs.WriteAPI.Application.DTOs;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.FeatureAddCar;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.FeatureDeleteCar;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.FeatureUpdateCar;
 using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 using RentACarNow.Common.Events.Feature;
 
@@ -13,6 +16,16 @@ namespace RentACarNow.APIs.WriteAPI.Application.Mapping
             CreateMap<Feature, FeatureAddedCarEvent>();
             CreateMap<Feature, FeatureDeletedEvent>();
             CreateMap<Feature, FeatureUpdatedEvent>();
+
+            CreateMap<FeatureAddCarCommandRequest, Feature>();
+            CreateMap<FeatureUpdateCarCommandRequest, Feature>()
+                .ForMember(d => d.Id,
+                opt => opt.MapFrom(src => src.FeatureId));
+
+            CreateMap<FeatureDeleteCarCommandRequest, Feature>()
+                 .ForMember(d => d.Id,
+                opt => opt.MapFrom(src => src.FeatureId));
+            ;
 
             CreateMap<FeatureDTO, Feature>();
 
