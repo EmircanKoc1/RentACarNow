2b55f947dc992971a74c85f547895a75a469a718
Emircan <emircandv787@gmail.com>
2024-08-28 22:26:52 +0300

    The CustomerMessage has been deleted and UserMessage has been created .

---
+++ src/Common/RentACarNow.Common.Events/Common/Messages/UserMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Events.Common.Messages
+{
+    public class UserMessage
+    {
+        public string Name { get; init; }
+        public string Surname { get; init; }
+        public int Age { get; init; }
+        public string PhoneNumber { get; init; }
+        public string Email { get; init; }
+    }
+}
