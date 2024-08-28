b668051ae870322a2375893c61ab01671ddf43a9
Emircan <emircandv787@gmail.com>
2024-08-28 18:44:41 +0300

    The AddClaimToUser and DeleteClaimToUser methods have been defined and implemented.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Repositories/Write/EfCore/IEfCoreUserWriteRepository.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Repositories/Write/EfCore/IEfCoreUserWriteRepository.cs
@@ -5,5 +5,7 @@ namespace RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore
 {
     public interface IEfCoreUserWriteRepository : IEfWriteRepository<User>
     {
+        Task AddClaimToUser(Guid userId , Guid claimId);
+        Task DeleteClaimToUser(Guid userId , Guid claimId);
     }
 }
--- src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreUserWriteRepository.cs
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreUserWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
+﻿using Microsoft.EntityFrameworkCore;
+using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 using RentACarNow.APIs.WriteAPI.Persistence.Contexts;
 using RentACarNow.APIs.WriteAPI.Persistence.Repositories.Base;
@@ -9,6 +10,27 @@ namespace RentACarNow.APIs.WriteAPI.Persistence.Repositories.Write.EfCore
     {
         public EfCoreUserWriteRepository(RentalACarNowDbContext context) : base(context)
         {
+        }
+
+        public async Task AddClaimToUser(Guid userId, Guid claimId)
+        {
+            var user = await _context.Users
+                .Include(u => u.Claims)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+            var claim = await _context.Claims.FindAsync(claimId);
+
+            user.Claims.Add(claim);
+        }
+
+        public async Task DeleteClaimToUser(Guid userId, Guid claimId)
+        {
+            var user = await _context.Users
+                .Include(u => u.Claims)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            var claim = await _context.Claims.FindAsync(claimId);
+
+            user.Claims.Remove(claim);
         }
     }
 }
