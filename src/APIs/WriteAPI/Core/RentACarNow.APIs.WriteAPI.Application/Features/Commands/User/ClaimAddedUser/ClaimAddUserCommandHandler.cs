d8d4fc23ad6cd1db7b4e76e06a4bce285933581b
Emircan <emircandv787@gmail.com>
2024-08-28 18:45:35 +0300

    The ClaimAddUserCommandHandler has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimAddedUser/ClaimAddUserCommandHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimAddedUser/ClaimAddUserCommandHandler.cs
@@ -4,6 +4,11 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
+using RentACarNow.Common.Entities.OutboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Events.Common.Messages;
+using RentACarNow.Common.Events.User;
+using RentACarNow.Common.Infrastructure.Extensions;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAddedUser
@@ -14,7 +19,6 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAdde
         private readonly IEfCoreUserWriteRepository _userWriteRepository;
         private readonly IEfCoreUserReadRepository _userReadRepository;
         private readonly IEfCoreClaimReadRepository _claimReadRepository;
-        private readonly IEfCoreClaimWriteRepository _claimWriteRepository;
         private readonly IUserOutboxRepository _userOutboxRepository;
         private readonly ILogger<ClaimAddUserCommandHandler> _logger;
         private readonly IValidator<ClaimAddUserCommandRequest> _validator;
@@ -27,16 +31,15 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAdde
             IUserOutboxRepository userOutboxRepository,
             ILogger<ClaimAddUserCommandHandler> logger,
             IValidator<ClaimAddUserCommandRequest> validator,
-            IMapper mapper,
-            IEfCoreClaimWriteRepository claimWriteRepository)
+            IMapper mapper)
         {
             _userWriteRepository = userWriteRepository;
             _userReadRepository = userReadRepository;
+            _claimReadRepository = claimReadRepository;
             _userOutboxRepository = userOutboxRepository;
             _logger = logger;
             _validator = validator;
             _mapper = mapper;
-            _claimWriteRepository = claimWriteRepository;
         }
 
         public async Task<ClaimAddUserCommandResponse> Handle(ClaimAddUserCommandRequest request, CancellationToken cancellationToken)
@@ -49,14 +52,48 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAdde
             }
 
             var efClaim = await _claimReadRepository.GetByIdAsync(request.ClaimId);
-            var efUser = await _userReadRepository.GetByIdAsync(request.UserId,true);
+            var userIsExists = await _userReadRepository.IsExistsAsync(request.UserId);
 
-            if (efClaim is null || efUser is null)
+            if (efClaim is null || !userIsExists)
                 return null;
 
-            efUser.Claims.Add(efClaim);
 
-            await _userWriteRepository.SaveChangesAsync();
+            using var efTran = await _userWriteRepository.BeginTransactionAsync();
+            using var mongoSession = await _userOutboxRepository.StartSessionAsync();
+
+
+            try
+            {
+                mongoSession.StartTransaction();
+
+                await _userWriteRepository.AddClaimToUser(request.UserId, request.ClaimId);
+                await _userWriteRepository.SaveChangesAsync();
+
+                await _userOutboxRepository.AddMessageAsync(new UserOutboxMessage
+                {
+                    AddedDate = DateTime.Now,
+                    EventType = UserEventType.UserClaimAddedEvent,
+                    Id = Guid.NewGuid(),
+                    Payload = new UserClaimAddedEvent
+                    {
+                        UserId = request.UserId,
+                        Claim = _mapper.Map<ClaimMessage>(efClaim)
+                    }.Serialize()!
+                }, mongoSession);
+
+
+
+
+                await mongoSession.CommitTransactionAsync();
+                await efTran.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await mongoSession.AbortTransactionAsync();
+                await efTran.RollbackAsync();
+
+                throw;
+            }
 
             return null;
 
