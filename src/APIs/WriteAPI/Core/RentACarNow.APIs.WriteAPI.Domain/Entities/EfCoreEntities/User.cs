37515951da48c25b2155481062853013ef086364
Emircan <emircandv787@gmail.com>
2024-08-29 11:35:32 +0300

    The User entity has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Domain/Entities/EfCoreEntities/User.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Domain/Entities/EfCoreEntities/User.cs
@@ -10,7 +10,6 @@ namespace RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities
         public string Surname { get; set; }
         public int Age { get; set; }
         public string PhoneNumber { get; set; }
-        //public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
 
