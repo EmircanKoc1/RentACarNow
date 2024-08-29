995610c5dc5e66022c97e4872fb68f59b29fda6a
Emircan <emircandv787@gmail.com>
2024-08-29 14:07:09 +0300

    The IMongoRentalOutboxContext and MongoRentalOutboxContext have been defined.

---
+++ src/Common/RentACarNow.Common.Contexts/OutboxContexts/Implementations/MongoRentalOutboxContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Interfaces;
+using RentACarNow.Common.Entities.OutboxEntities;
+
+namespace RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations
+{
+    public class MongoRentalOutboxContext : BaseMongoOutboxContext<RentalOutboxMessage> ,IMongoRentalOutboxContext
+    {
+        public MongoRentalOutboxContext(MongoClient mongoClient, string databaseName) : base(mongoClient, databaseName)
+        {
+        }
+
+        protected sealed override string OutboxName => "RentalOutbox";
+    }
+}
---
+++ src/Common/RentACarNow.Common.Contexts/OutboxContexts/Interfaces/IMongoRentalOutboxContext.cs
@@ -0,0 +1,8 @@
+﻿using RentACarNow.Common.Entities.OutboxEntities;
+
+namespace RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Interfaces
+{
+    public interface IMongoRentalOutboxContext : IBaseMongoOutboxContext<RentalOutboxMessage>
+    {
+    }
+}
