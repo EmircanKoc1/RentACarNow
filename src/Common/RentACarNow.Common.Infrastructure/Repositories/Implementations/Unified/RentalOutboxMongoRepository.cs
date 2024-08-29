f33067f88e46532f787b51e4ca63293ad6a31062
Emircan <emircandv787@gmail.com>
2024-08-29 14:23:34 +0300

    The IRentalOutboxMongoRepository and IRentalOutboxRepository have been defined and implemented.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Implementations/Unified/RentalOutboxMongoRepository.cs
@@ -0,0 +1,17 @@
+﻿using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations;
+using RentACarNow.Common.Entities.OutboxEntities;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Base;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+
+
+namespace RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified
+{
+    public class RentalOutboxMongoRepository : BaseOutboxMongoRepository<RentalOutboxMessage, MongoRentalOutboxContext>, IRentalOutboxRepository
+    {
+        public RentalOutboxMongoRepository(MongoRentalOutboxContext context) : base(context)
+        {
+
+        }
+
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Interfaces/Unified/IRentalOutboxRepository.cs
@@ -0,0 +1,11 @@
+﻿using RentACarNow.Common.Entities.OutboxEntities;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Base;
+
+namespace RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified
+{
+    public interface IRentalOutboxRepository : IBaseOutboxRepository<RentalOutboxMessage>
+    {
+
+    }
+
+}
