83b8e739d2b8c99bbe5f0bfee831644730e03b92
Emircan <emircandv787@gmail.com>
2024-08-29 11:35:52 +0300

    The User entity(mongo) has been refactored.

--- src/Common/RentACarNow.Common.Entities/MongoEntities/User.cs
+++ src/Common/RentACarNow.Common.Entities/MongoEntities/User.cs
@@ -1,4 +1,5 @@
-﻿using RentACarNow.Common.MongoEntities.Common.Concrete;
+﻿using RentACarNow.Common.MongoEntities;
+using RentACarNow.Common.MongoEntities.Common.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,16 @@ namespace RentACarNow.Common.Entities.MongoEntities
     public class User : MongoBaseEntity
     {
 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
 
+        public decimal WalletBalance { get; set; }
+
+        public ICollection<Claim> Claims { get; set; }
 
     }
 }
