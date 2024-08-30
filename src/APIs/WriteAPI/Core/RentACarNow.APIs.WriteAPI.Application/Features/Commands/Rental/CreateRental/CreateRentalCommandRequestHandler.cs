e49dd85e51ef8e5015a0847c4e1ed7802549f5ac
Emircan <emircandv787@gmail.com>
2024-08-30 21:09:03 +0300

    The CreateRentalCommandRequestHandler has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/CreateRental/CreateRentalCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/CreateRental/CreateRentalCommandRequestHandler.cs
@@ -3,7 +3,13 @@ using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using RentACarNow.APIs.WriteAPI.Application.Interfaces.UnitOfWorks;
+using RentACarNow.Common.Entities.OutboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Events.Common.Messages;
 using RentACarNow.Common.Events.Rental;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using ZstdSharp.Unsafe;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.CreateRental
@@ -11,6 +17,7 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.CreateR
     public class CreateRentalCommandRequestHandler : IRequestHandler<CreateRentalCommandRequest, CreateRentalCommandResponse>
     {
         private readonly IRentalUnitOfWork _rentalUnitOfWork;
+        private readonly IRentalOutboxRepository _rentalOutboxRepository;
         private readonly IValidator<CreateRentalCommandRequest> _validator;
         private readonly ILogger<CreateRentalCommandRequestHandler> _logger;
         private readonly IMapper _mapper;
@@ -19,12 +26,14 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.CreateR
             IRentalUnitOfWork rentakUnitOfWork,
             IValidator<CreateRentalCommandRequest> validator,
             ILogger<CreateRentalCommandRequestHandler> logger,
-            IMapper mapper)
+            IMapper mapper,
+            IRentalOutboxRepository rentalOutboxRepository)
         {
             _rentalUnitOfWork = rentakUnitOfWork;
             _validator = validator;
             _logger = logger;
             _mapper = mapper;
+            _rentalOutboxRepository = rentalOutboxRepository;
         }
 
         public async Task<CreateRentalCommandResponse> Handle(CreateRentalCommandRequest request, CancellationToken cancellationToken)
@@ -36,23 +45,43 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.CreateR
                 return new CreateRentalCommandResponse();
             }
 
-            var efRentalEntity = _mapper.Map<EfEntity.Rental>(request);
+            var foundedUser = await _rentalUnitOfWork.UserReadRepository.GetByIdAsync(request.UserId);
+            var foundedCar = await _rentalUnitOfWork.CarReadRepository.GetByIdAsync(request.CarId);
 
+            if (foundedCar is null || foundedUser is null)
+                return new CreateRentalCommandResponse();
+
+
+            var efRentalEntity = _mapper.Map<EfEntity.Rental>(request);
             efRentalEntity.Id = Guid.NewGuid();
 
+            var rentalCreatedEvent = _mapper.Map<RentalCreatedEvent>(request);
+            rentalCreatedEvent.Car = _mapper.Map<CarMessage>(foundedCar);
+            rentalCreatedEvent.User = _mapper.Map<UserMessage>(foundedUser);
+
+
+            using var mongoSession = await _rentalOutboxRepository.StartSessionAsync();
             try
             {
+                mongoSession.StartTransaction();
                 await _rentalUnitOfWork.BeginTransactionAsync();
-                
 
                 await _rentalUnitOfWork.RentalWriteRepository.AddAsync(efRentalEntity);
+                await _rentalOutboxRepository.AddMessageAsync(new RentalOutboxMessage()
+                {
+                    Id = Guid.NewGuid(),
+                    AddedDate = DateTime.UtcNow,
+                    EventType = RentalEventType.RentalCreatedEvent,
+                    Payload = rentalCreatedEvent.Serialize()!
+                }, mongoSession);
 
-
+                await mongoSession.CommitTransactionAsync();
                 await _rentalUnitOfWork.CommitAsync();
             }
             catch (Exception)
             {
                 await _rentalUnitOfWork.RollbackAsync();
+                await mongoSession.AbortTransactionAsync();
                 throw;
             }
 
