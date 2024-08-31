dbbaecdb5e16c2cd238c4a45fdde7cfc43766de7
Emircan <emircandv787@gmail.com>
2024-08-31 15:01:28 +0300

    The RabbitMQQueues has been refactored .

--- src/Common/RentACarNow.Common.Constants/MessageBrokers/Queues/RabbitMQQueues.cs
+++ src/Common/RentACarNow.Common.Constants/MessageBrokers/Queues/RabbitMQQueues.cs
@@ -2,9 +2,6 @@
 {
     public static class RabbitMQQueues
     {
-        public const string ADMIN_ADDED_QUEUE = "ADMIN_ADDED_QUEUE";
-        public const string ADMIN_DELETED_QUEUE = "ADMIN_DELETED_QUEUE";
-        public const string ADMIN_UPDATED_QUEUE = "ADMIN_UPDATED_QUEUE";
 
 
         public const string BRAND_ADDED_QUEUE = "BRAND_ADDED_QUEUE";
@@ -15,37 +12,28 @@
         public const string CAR_ADDED_QUEUE = "CAR_ADDED_QUEUE";
         public const string CAR_DELETED_QUEUE = "CAR_DELETED_QUEUE";
         public const string CAR_UPDATED_QUEUE = "CAR_UPDATED_QUEUE";
-
-
-        public const string CUSTOMER_ADDED_QUEUE = "CUSTOMER_ADDED_QUEUE";
-        public const string CUSTOMER_DELETED_QUEUE = "CUSTOMER_DELETED_QUEUE";
-        public const string CUSTOMER_UPDATED_QUEUE = "CUSTOMER_UPDATED_QUEUE";
-
-
-        public const string EMPLOYEE_ADDED_QUEUE = "EMPLOYEE_ADDED_QUEUE";
-        public const string EMPLOYEE_DELETED_QUEUE = "EMPLOYEE_DELETED_QUEUE";
-        public const string EMPLOYEE_UPDATED_QUEUE = "EMPLOYEE_UPDATED_QUEUE";
-
-
-        public const string FEATURE_ADDED_QUEUE = "FEATURE_ADDED_QUEUE";
-        public const string FEATURE_DELETED_QUEUE = "FEATURE_DELETED_QUEUE";
-        public const string FEATURE_UPDATED_QUEUE = "FEATURE_UPDATED_QUEUE";
+        public const string CAR_FEATURE_ADDED_QUEUE = "CAR_FEATURE_ADDED_QUEUE";
+        public const string CAR_FEATURE_DELETED_QUEUE = "CAR_FEATURE_DELETED_QUEUE";
+        public const string CAR_FEATURE_UPDATED_QUEUE = "CAR_FEATURE_UPDATED_QUEUE";
 
 
         public const string RENTAL_ADDED_QUEUE = "RENTAL_ADDED_QUEUE";
         public const string RENTAL_DELETED_QUEUE = "RENTAL_DELETED_QUEUE";
         public const string RENTAL_UPDATED_QUEUE = "RENTAL_UPDATED_QUEUE";
 
+        public const string USER_CREATED_QUEUE = "USER_CREATED_QUEUE ";
+        public const string USER_DELETED_QUEUE = "USER_DELETED_QUEUE";
+        public const string USER_UPDATED_QUEUE = "USER_UPDATED_QUEUE ";
+        public const string USER_CLAIM_DELETED_QUEUE = "USER_CLAIM_DELETED_QUEUE ";
+        public const string USER_CLAIM_ADDED_QUEUE = "USER_CLAIM_ADDED_QUEUE ";
+
+
 
         public const string CLAIM_ADDED_QUEUE = "CLAIM_ADDED_QUEUE";
         public const string CLAIM_DELETED_QUEUE = "CLAIM_DELETED_QUEUE";
         public const string CLAIM_UPDATED_QUEUE = "CLAIM_UPDATED_QUEUE";
-        public const string CLAIM_ADDED_TO_ADMIN_QUEUE = "CLAIM_ADDED_TO_ADMIN_QUEUE";
-        public const string CLAIM_ADDED_TO_EMPLOYEE_QUEUE = "CLAIM_ADDED_TO_EMPLOYEE_QUEUE";
-        public const string CLAIM_ADDED_TO_CUSTOMER_QUEUE = "CLAIM_ADDED_TO_CUSTOMER_QUEUE";
-        public const string CLAIM_DELETED_FROM_ADMIN_QUEUE = "CLAIM_DELETED_FROM_ADMIN_QUEUE";
-        public const string CLAIM_DELETED_FROM_CUSTOMER_QUEUE = "CLAIM_DELETED_FROM_CUSTOMER_QUEUE";
-        public const string CLAIM_DELETED_FROM_EMPLOYEE_QUEUE = "CLAIM_DELETED_FROM_EMPLOYEE_QUEUE";
+
+
 
 
     }
