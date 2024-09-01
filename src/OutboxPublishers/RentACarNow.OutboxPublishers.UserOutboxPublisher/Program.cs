2e78324b4ef1d0c54f440dea86afa1079bda2612
Emircan <emircandv787@gmail.com>
2024-09-01 18:00:31 +0300

    The MongoUserOutboxContext , UserPublisherService , UserOutboxRepository , and IRabbitMQMessageService have been added to DI container .

--- src/OutboxPublishers/RentACarNow.OutboxPublishers.UserOutboxPublisher/Program.cs
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.UserOutboxPublisher/Program.cs
@@ -1,7 +1,26 @@
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.OutboxPublishers.UserOutboxPublisher;
+using RentACarNow.Common.Infrastructure.Extensions;
+
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+
+builder.Services.AddSingleton<MongoUserOutboxContext>(x =>
+{
+    return new MongoUserOutboxContext(new MongoClient(MongoDbConstants.CONNECTION_STRING), "OutboxDB");
+});
+
+builder.Services.AddHostedService<UserPublisherService>();
+
+builder.Services.AddSingleton<IUserOutboxRepository, UserOutboxMongoRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "useroutboxpublisher");
+
 
 var host = builder.Build();
 host.Run();
