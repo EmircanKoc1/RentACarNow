fc856c385fb97e57ec74466894a61cdf24daff17
Emircan <emircandv787@gmail.com>
2024-09-01 15:24:28 +0300

    The ClaimPublisherService has been created and MongoClaimOutboxContext , IClaimOutboxRepository , and RabbitMqMessageService have been added to DI container .

---
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.ClaimOutboxPublisher/ClaimPublisherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.OutboxPublishers.ClaimOutboxPublisher
+{
+    public class ClaimPublisherService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/OutboxPublishers/RentACarNow.OutboxPublishers.ClaimOutboxPublisher/Program.cs
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.ClaimOutboxPublisher/Program.cs
@@ -1,7 +1,26 @@
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.OutboxPublishers.ClaimOutboxPublisher;
+using RentACarNow.Common.Infrastructure.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddHostedService<ClaimPublisherService>();
+
+
+builder.Services.AddSingleton<MongoClaimOutboxContext>(x =>
+{
+    return new MongoClaimOutboxContext(new MongoClient(MongoDbConstants.CONNECTION_STRING), "OutboxDB");
+});
+
+
+builder.Services.AddSingleton<IClaimOutboxRepository, ClaimOutboxMongoRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "brandoutboxpublisher");
+
 
 var host = builder.Build();
 host.Run();
