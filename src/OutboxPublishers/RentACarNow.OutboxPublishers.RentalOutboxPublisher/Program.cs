8af8a55035ebbcfccec7e4fdf1749077af5dd611
Emircan <emircandv787@gmail.com>
2024-09-01 14:04:33 +0300

    The RentalOutboxPublisherService has been completed.

--- src/OutboxPublishers/RentACarNow.OutboxPublishers.RentalOutboxPublisher/Program.cs
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.RentalOutboxPublisher/Program.cs
@@ -1,7 +1,29 @@
+
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.OutboxContexts.MongoContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.OutboxPublishers.RentalOutboxPublisher;
+using RentACarNow.Common.Infrastructure.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+
+
+builder.Services.AddSingleton<MongoRentalOutboxContext>(x =>
+{
+    return new MongoRentalOutboxContext(new MongoClient(MongoDbConstants.CONNECTION_STRING), "OutboxDB");
+});
+
+builder.Services.AddHostedService<RentalPublisherService>();
+
+builder.Services.AddSingleton<IRentalOutboxRepository, RentalOutboxMongoRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "rentalboxpublisher");
+
+
+
 
 var host = builder.Build();
 host.Run();
---
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.RentalOutboxPublisher/RentalPublisherService.cs
@@ -0,0 +1,97 @@
+﻿using RentACarNow.Common.Constants.MessageBrokers.Exchanges;
+using RentACarNow.Common.Constants.MessageBrokers.RoutingKeys;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Enums.RepositoryEnums;
+using RentACarNow.Common.Events.Rental;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Infrastructure.Extensions;
+
+namespace RentACarNow.OutboxPublishers.RentalOutboxPublisher
+{
+    public class RentalPublisherService : BackgroundService
+    {
+        private readonly IRabbitMQMessageService _rabbitMQMessageService;
+        private readonly ILogger<RentalPublisherService> _logger;
+        private readonly IRentalOutboxRepository _rentalOutboxRepository;
+
+        public RentalPublisherService(
+            IRabbitMQMessageService rabbitMQMessageService,
+            ILogger<RentalPublisherService> logger,
+            IRentalOutboxRepository rentalOutboxRepository)
+        {
+            _rabbitMQMessageService = rabbitMQMessageService;
+            _logger = logger;
+            _rentalOutboxRepository = rentalOutboxRepository;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"{nameof(RentalPublisherService)} is executed ");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+                var outboxMessages = await _rentalOutboxRepository.GetOutboxMessagesAsync(5, OrderedDirection.None);
+
+
+                foreach (var message in outboxMessages)
+                {
+
+                    var messagePayload = message.Payload;
+                    var date = DateTime.Now;
+
+                    switch (message.EventType)
+                    {
+                        case RentalEventType.RentalCreatedEvent:
+
+
+                            var rentalCreatedEvent = messagePayload.Deseralize<RentalCreatedEvent>();
+
+                            _rabbitMQMessageService.SendEventQueue<RentalCreatedEvent>(
+                                exchangeName: RabbitMQExchanges.RENTAL_EXCHANGE,
+                                routingKey: RabbitMQRoutingKeys.RENTAL_ADDED_ROUTING_KEY,
+                                @event: rentalCreatedEvent);
+
+                            await _rentalOutboxRepository.MarkPublishedMessageAsync(message.Id, date);
+
+                            break;
+                        case RentalEventType.RentalUpdatedEvent:
+
+                            var rentalUpdateEvent = messagePayload.Deseralize<RentalUpdatedEvent>();
+
+                            _rabbitMQMessageService.SendEventQueue<RentalUpdatedEvent>(
+                                exchangeName: RabbitMQExchanges.RENTAL_EXCHANGE,
+                                routingKey: RabbitMQRoutingKeys.RENTAL_UPDATED_ROUTING_KEY,
+                                @event: rentalUpdateEvent);
+
+                            await _rentalOutboxRepository.MarkPublishedMessageAsync(message.Id, date);
+
+                            break;
+                        case RentalEventType.RentalDeletedEvent:
+
+                            var rentalDeletedEvent = messagePayload.Deseralize<RentalDeletedEvent>();
+
+                            _rabbitMQMessageService.SendEventQueue<RentalDeletedEvent>(
+                                exchangeName: RabbitMQExchanges.RENTAL_EXCHANGE,
+                                routingKey: RabbitMQRoutingKeys.RENTAL_DELETED_ROUTING_KEY,
+                                @event: rentalDeletedEvent);
+
+                            await _rentalOutboxRepository.MarkPublishedMessageAsync(message.Id, date);
+
+                            break;
+                        default:
+                            _logger.LogInformation($"\"The event type didn't match any event\" , id : {message.Id} , event type : {message.EventType}");
+                            break;
+                    }
+                    _logger.LogInformation(messagePayload);
+
+
+
+                }
+
+            }
+
+        }
+    }
+}
