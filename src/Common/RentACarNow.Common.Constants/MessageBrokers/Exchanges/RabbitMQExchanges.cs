69e9cc2ae1c6f17a2e08a3362a6ed68ea05ecb27
Emircan <emircandv787@gmail.com>
2024-09-01 18:02:51 +0300

    The Exchanges has been refactored .

--- src/Common/RentACarNow.Common.Constants/MessageBrokers/Exchanges/RabbitMQExchanges.cs
+++ src/Common/RentACarNow.Common.Constants/MessageBrokers/Exchanges/RabbitMQExchanges.cs
@@ -3,14 +3,12 @@
     public static class RabbitMQExchanges
     {
 
-        public const string ADMIN_EXCHANGE = "ADMIN_EXCHANGE";
         public const string BRAND_EXCHANGE = "BRAND_EXCHANGE";
         public const string CAR_EXCHANGE = "CAR_EXCHANGE";
-        public const string CUSTOMER_EXCHANGE = "CUSTOMER_EXCHANGE";
-        public const string EMPLOYEE_EXCHANGE = "EMPLOYEE_EXCHANGE";
         public const string FEATURE_EXCHANGE = "FEATURE_EXCHANGE";
         public const string RENTAL_EXCHANGE = "RENTAL_EXCHANGE";
         public const string CLAIM_EXCHANGE = "CLAIM_EXCHANGE";
+        public const string USER_EXCHANGE = "USER_EXCHANGE";
 
 
     }
