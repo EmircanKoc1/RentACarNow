095eb6ed94d903520f8db52e5215b3e0906a92e6
Emircan <emircandv787@gmail.com>
2024-09-02 16:02:58 +0300

    The ClaimService has been created.

---
+++ src/Projections/RentACarNow.Projections.ClaimService/Program.cs
@@ -0,0 +1,7 @@
+using RentACarNow.Projections.ClaimService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
