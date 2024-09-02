1f9400c0470bad7f7232eea7c4f9ebb16d368471
Emircan <emircandv787@gmail.com>
2024-09-02 16:26:28 +0300

    The RentalService has been created.

---
+++ src/Projections/RentACarNow.Projections.RentalService/Program.cs
@@ -0,0 +1,7 @@
+using RentACarNow.Projections.RentalService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
