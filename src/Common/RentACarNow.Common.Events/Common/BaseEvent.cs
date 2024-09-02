4ddd8853d74098f4ffb302a05d89b692c1013c33
Emircan <emircandv787@gmail.com>
2024-09-02 19:56:38 +0300

    The IdempotentId has been added to the BaseEvent

--- src/Common/RentACarNow.Common.Events/Common/BaseEvent.cs
+++ src/Common/RentACarNow.Common.Events/Common/BaseEvent.cs
@@ -6,6 +6,7 @@ namespace RentACarNow.Common.Events.Common
 
     public class BaseEvent : IEvent
     {
+        public Guid IdempotentId { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
