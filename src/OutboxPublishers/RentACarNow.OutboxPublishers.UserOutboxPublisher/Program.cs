963c4c268c68cd829039589e2d0703019ebd7448
Emircan <emircandv787@gmail.com>
2024-09-02 15:20:18 +0300

    The CreateExchanges ,CreateQueues, and BindExchangesAndQueues methods have been called in the UserOutboxPublisher.

--- src/OutboxPublishers/RentACarNow.OutboxPublishers.UserOutboxPublisher/Program.cs
+++ src/OutboxPublishers/RentACarNow.OutboxPublishers.UserOutboxPublisher/Program.cs
@@ -5,9 +5,11 @@ using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.OutboxPublishers.UserOutboxPublisher;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
 
 
 var builder = Host.CreateApplicationBuilder(args);
+
 
 
 builder.Services.AddSingleton<MongoUserOutboxContext>(x =>
@@ -21,6 +23,12 @@ builder.Services.AddSingleton<IUserOutboxRepository, UserOutboxMongoRepository>(
 
 builder.Services.AddIRabbitMQMessageService(clientName: "useroutboxpublisher");
 
-
 var host = builder.Build();
+
+host.Services.GetService<IRabbitMQMessageService>()?.CreateExchanges();
+host.Services.GetService<IRabbitMQMessageService>()?.CreateQueues();
+host.Services.GetService<IRabbitMQMessageService>()?.BindExchangesAndQueues();
+
+
+
 host.Run();
