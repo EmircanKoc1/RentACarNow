20aa208d83d2e8f85d30cf1a28f5c4a6c3370eff
Emircan <emircandv787@gmail.com>
2024-09-03 16:19:37 +0300

    The BrandMapProfile has been added .

---
+++ src/Projections/RentACarNow.Projections.BrandService/Mappings/BrandMapProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Events.Brand;
+using RentACarNow.Common.MongoEntities;
+
+namespace RentACarNow.Projections.BrandService.Mappings
+{
+    public class BrandMapProfile : Profile
+    {
+        public BrandMapProfile()
+        {
+            CreateMap<BrandCreatedEvent, Brand>();
+            CreateMap<BrandDeletedEvent, Brand>();
+            CreateMap<BrandUpdatedEvent, Brand>();
+        }
+
+    }
+}
