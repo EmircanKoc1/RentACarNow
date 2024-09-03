9e09049c573c4db11f1505484874dbca2204b071
Emircan <emircandv787@gmail.com>
2024-09-03 21:21:45 +0300

    New Features and controls have been added to DeleteBrandCommandRequestHandler.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Brand/DeleteBrand/DeleteBrandCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Brand/DeleteBrand/DeleteBrandCommandRequestHandler.cs
@@ -7,8 +7,10 @@ using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.Common.Entities.OutboxEntities;
 using RentACarNow.Common.Events.Brand;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
-using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Models;
+using System.Net;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.DeleteBrand
 {
@@ -39,56 +41,96 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.DeleteBr
 
         public async Task<DeleteBrandCommandResponse> Handle(DeleteBrandCommandRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogDebug($"{nameof(DeleteBrandCommandRequestHandler)} Handle method has been executed");
+
             var validationResult = await _validator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
-                return new DeleteBrandCommandResponse { };
+                _logger.LogInformation($"{nameof(DeleteBrandCommandRequestHandler)} Request not validated");
+
+                return new DeleteBrandCommandResponse
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = validationResult.Errors?.Select(vf => new ResponseErrorModel
+                    {
+                        PropertyName = vf.PropertyName,
+                        ErrorMessage = vf.ErrorMessage
+                    })
+                };
+
             }
 
             var isExists = await _brandReadRepository.IsExistsAsync(request.Id);
 
             if (!isExists)
-                return new DeleteBrandCommandResponse { };
+            {
+                _logger.LogInformation($"{nameof(DeleteBrandCommandRequestHandler)} Entity not found , id : {request.Id}");
+                return new DeleteBrandCommandResponse
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "Entity not found",
+                            PropertyName = null
+                        }
+}
+                };
+            }
 
             using var efTransaction = await _brandWriteRepository.BeginTransactionAsync();
             using var mongoSession = await _brandOutboxRepository.StartSessionAsync();
+
+            _logger.LogInformation($"{nameof(DeleteBrandCommandRequestHandler)} Transaction started");
 
             mongoSession.StartTransaction();
 
             var brandDeletedEvent = new BrandDeletedEvent
             {
-                DeletedDate = DateTime.UtcNow,
+                DeletedDate = DateHelper.GetDate(),
                 Id = request.Id,
             };
 
+            brandDeletedEvent.MessageId = Guid.NewGuid();
+
             try
             {
+
                 _brandWriteRepository.DeleteById(request.Id);
                 _brandWriteRepository.SaveChanges();
 
-                await _brandOutboxRepository.AddMessageAsync(
-                      new BrandOutboxMessage
-                      {
-                          Id = Guid.NewGuid(),
-                          AddedDate = DateTime.Now,
-                          IsPublished = false,
-                          PublishDate = null,
-                          Payload = brandDeletedEvent.Serialize()!
-                      }, mongoSession);
+                var outboxMessage = new BrandOutboxMessage
+                {
+                    Id = brandDeletedEvent.MessageId,
+                    AddedDate = DateHelper.GetDate(),
+                    Payload = brandDeletedEvent.Serialize()!
+                };
+
+                await _brandOutboxRepository.AddMessageAsync(outboxMessage, mongoSession);
 
                 efTransaction.Commit();
                 await mongoSession.CommitTransactionAsync();
+
+                _logger.LogInformation($"{nameof(DeleteBrandCommandRequestHandler)} Transaction commited");
             }
             catch (Exception)
             {
+
+
                 await efTransaction.RollbackAsync();
                 await mongoSession.AbortTransactionAsync();
-                throw;
+
+                _logger.LogError($"{nameof(DeleteBrandCommandRequestHandler)} transaction rollbacked");
             }
 
 
-            return new DeleteBrandCommandResponse { };
+            return new DeleteBrandCommandResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Errors = null
+            };
         }
     }
 
