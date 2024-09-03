34edbbe3562e14be2da07f0473e52845cb39fb59
Emircan <emircandv787@gmail.com>
2024-09-03 16:16:48 +0300

    The BsonId attribute has been added to the MessageId property .

--- src/Common/RentACarNow.Common.Entities/InboxEntities/BaseInboxMessage.cs
+++ src/Common/RentACarNow.Common.Entities/InboxEntities/BaseInboxMessage.cs
@@ -1,7 +1,10 @@
-﻿namespace RentACarNow.Common.Entities.InboxEntities
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace RentACarNow.Common.Entities.InboxEntities
 {
     public abstract class BaseInboxMessage : IInboxMessage
     {
+        [BsonId]
         public Guid MessageId { get; set; }
         public string Payload { get; set; }
         public bool IsProcessed { get; set; }
