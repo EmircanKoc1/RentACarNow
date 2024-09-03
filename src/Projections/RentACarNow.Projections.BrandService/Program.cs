b7d79019054cbeb54d6bc6db7170ae4e66ac071c
Emircan <emircandv787@gmail.com>
2024-09-03 17:06:25 +0300

    The services have been registered the DI container.

--- src/Projections/RentACarNow.Projections.BrandService/Program.cs
+++ src/Projections/RentACarNow.Projections.BrandService/Program.cs
@@ -1,7 +1,38 @@
+
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.Projections.BrandService;
+using RentACarNow.Projections.BrandService.Consumers;
+using RentACarNow.Common.Infrastructure.Extensions;
+using System.Reflection;
+using RentACarNow.Common.Contexts.InboxContexts.Implementations;
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Write.Mongo;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+
+
+
+builder.Services.AddSingleton<MongoBrandInboxContext>(p =>
+{
+    return new MongoBrandInboxContext(
+    mongoClient: new MongoClient(MongoDbConstants.CONNECTION_STRING),
+    databaseName: "InboxDB");
+});
+
+builder.Services.AddSingleton<IBrandInboxRepository, BrandInboxMongoRepository>();
+builder.Services.AddIRabbitMQMessageService(clientName:"BrandService");
+
+builder.Services.AddHostedService<BrandCreatedEventConsumer>();
+//builder.Services.AddHostedService<BrandDeletedEventConsumer>();
+//builder.Services.AddHostedService<BrandUpdatedEventConsumer>();
+
+builder.Services.AddMongoRentalACarNowDBContext();
+builder.Services.AddSingleton<IMongoBrandWriteRepository,MongoBrandWriteRepository>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var host = builder.Build();
 host.Run();
