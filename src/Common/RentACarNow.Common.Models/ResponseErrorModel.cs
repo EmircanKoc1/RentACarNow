7830cbca6177464446b64adfdce4ea5a90ec17fa
Emircan <emircandv787@gmail.com>
2024-09-03 19:42:41 +0300

    The ResponseErrorModel class has been created .

---
+++ src/Common/RentACarNow.Common.Models/ResponseErrorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentACarNow.Common.Models
+{
+    public class ResponseErrorModel
+    {
+        public string? PropertyName { get; set; } = null!;
+        //public IEnumerable<string> ErrorMessages { get; set; } = null!;
+        public string ErrorMessage { get; set; }
+    }
+}
