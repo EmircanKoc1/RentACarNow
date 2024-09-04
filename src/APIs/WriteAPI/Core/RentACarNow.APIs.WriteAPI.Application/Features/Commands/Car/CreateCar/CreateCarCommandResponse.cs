5771c4a9cc9611da4e4f9af4b9c9957fcc746073
Emircan <emircandv787@gmail.com>
2024-09-04 20:39:41 +0300

    New properties have been added to the CreateCarCommanResponse

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/CreateCar/CreateCarCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/CreateCar/CreateCarCommandResponse.cs
@@ -1,8 +1,14 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar
 {
     public class CreateCarCommandResponse
     {
-        // İsteğe bağlı olarak oluşturma sonucuyla ilgili bilgiler eklenebilir
+        public Guid CarId { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
+
     }
 
 }
