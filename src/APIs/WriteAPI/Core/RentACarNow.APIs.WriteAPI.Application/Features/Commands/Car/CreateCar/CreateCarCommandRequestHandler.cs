33b42969ba87098ccf99810fbdfd7b212e3b0a17
Emircan <emircandv787@gmail.com>
2024-09-04 21:19:44 +0300

    The CreateCarCommandRequestHandler has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/CreateCar/CreateCarCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/CreateCar/CreateCarCommandRequestHandler.cs
@@ -2,6 +2,8 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.CreateBrand;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.UpdateBrand;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.Common.Entities.OutboxEntities;
@@ -9,8 +11,11 @@ using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.Car;
 using RentACarNow.Common.Events.Common.Messages;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Models;
+using System.Net;
 using EFEntities = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar
 {
@@ -44,27 +49,57 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar
 
         public async Task<CreateCarCommandResponse> Handle(CreateCarCommandRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogDebug($"{nameof(CreateCarCommandRequestHandler)} Handle method has been executed");
+
+
             var validationResult = await _validator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
-                return new CreateCarCommandResponse();
+                _logger.LogInformation($"{nameof(CreateCarCommandRequestHandler)} Request not validated");
+
+
+                return new CreateCarCommandResponse
+                {
+                    CarId = Guid.Empty,
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = validationResult.Errors?.Select(vf => new ResponseErrorModel
+                    {
+                        PropertyName = vf.PropertyName,
+                        ErrorMessage = vf.ErrorMessage
+                    })
+                };
             }
+            var foundedBrand = await _brandReadRepository.GetByIdAsync(request.BrandId);
 
-            var brand = await _brandReadRepository.GetByIdAsync(request.BrandId);
-
-            if (brand is null) return null;
+            if (foundedBrand is null)
+            {
+                _logger.LogInformation($"{nameof(UpdateBrandCommandRequestHandler)} brand not found , id : {request.BrandId}");
+                return new CreateCarCommandResponse
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "brand not found",
+                            PropertyName = null
+                        }
+                    }                   
+                };
+            }
 
             using var efTransaction = await _carWriteRepository.BeginTransactionAsync();
             using var mongoSession = await _carOutboxRepository.StartSessionAsync();
 
             var efCarEntity = _mapper.Map<EFEntities.Car>(request);
-            
             efCarEntity.Id = Guid.NewGuid();
+            efCarEntity.CreatedDate = DateHelper.GetDate();
+
 
             var carCreatedEvent = _mapper.Map<CarCreatedEvent>(efCarEntity);
 
-            carCreatedEvent.Brand = _mapper.Map<BrandMessage>(brand);
+            carCreatedEvent.Brand = _mapper.Map<BrandMessage>(foundedBrand);
 
             try
             {
@@ -78,25 +113,49 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar
                 await _carOutboxRepository.AddMessageAsync(new CarOutboxMessage()
                 {
                     Id = Guid.NewGuid(),
-                    AddedDate = DateTime.UtcNow,
+                    AddedDate = DateHelper.GetDate(),
                     CarEventType = CarEventType.CarCreatedEvent,
                     Payload = carCreatedEvent.Serialize()!,
-                    IsPublished = false,
-                    PublishDate = null
+                  
                 }, mongoSession);
 
 
                 await efTransaction.CommitAsync();
                 await mongoSession.CommitTransactionAsync();
+
+                _logger.LogInformation($"{nameof(CreateCarCommandRequestHandler)} Transaction commited");
+
+
             }
             catch
             {
 
                 await efTransaction.RollbackAsync();
                 await mongoSession.AbortTransactionAsync();
+
+                _logger.LogError($"{nameof(CreateCarCommandRequestHandler)} transaction rollbacked");
+
+                return new CreateCarCommandResponse
+                {
+
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "Transaction exception",
+                            PropertyName = null
+                        }
+                    }
+                };
             }
 
-            return null;
+            return new CreateCarCommandResponse
+            {
+                CarId = efCarEntity.Id,
+                StatusCode = HttpStatusCode.OK,
+                Errors = null
+            };
         }
     }
 }
