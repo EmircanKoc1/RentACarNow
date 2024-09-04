bff62f482a741f0644ab8b8f8723da63edd917de
Emircan <emircandv787@gmail.com>
2024-09-04 18:58:52 +0300

    The UpdateAsync Method has been overridden

--- src/Common/RentACarNow.Common.Infrastructure/Repositories/Implementations/Write/Mongo/MongoBrandWriteRepository.cs
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Implementations/Write/Mongo/MongoBrandWriteRepository.cs
@@ -16,32 +16,18 @@ namespace RentACarNow.Common.Infrastructure.Repositories.Implementations.Write.M
         {
             var filterDefination = Builders<Brand>.Filter.Eq(f => f.Id, entity.Id);
 
-            //var updateDefination = Builders<Brand>.Update
-            //    .Set(f => f.UpdatedDate, DateTime.Now)
-            //    .Set(f => f.Name, entity.Name)
-            //    .Set(f => f.Description, entity.Description);
-
-            //await _collection.UpdateOneAsync(filterDefination, updateDefination);
+            var updateDefiniton = Builders<Brand>.Update
+                .Set(f => f.Name, entity.Name)
+                .Set(f => f.Description, entity.Description)
+                .Set(f => f.UpdatedDate, entity.UpdatedDate);
 
 
-            await _collection.ReplaceOneAsync(filterDefination, entity);
+            await _collection.UpdateManyAsync(filterDefination, updateDefiniton);
 
 
         }
 
-        //public async Task UpdateCarInBrand(Guid brandId, Guid carId, Car entity)
-        //{
-        //    var filterDefinition = Builders<Brand>.Filter.And(
-        //        Builders<Brand>.Filter.Eq(brand => brand.Id, brandId),
-        //        Builders<Brand>.Filter.ElemMatch(brand => brand.Cars, car => car.Id == carId)
-        //        );
 
-        //    var updateDefinition = Builders<Brand>.Update
-        //        .Set("Cars.$", entity);
-
-        //    await _collection.UpdateOneAsync(filterDefinition, updateDefinition);
-
-        //}
 
     }
 
