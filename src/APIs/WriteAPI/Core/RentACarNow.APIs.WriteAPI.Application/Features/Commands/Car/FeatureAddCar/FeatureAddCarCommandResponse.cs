f3513f04732ceac702fc97ea28ab9e09f99bfc54
Emircan <emircandv787@gmail.com>
2024-09-05 15:22:50 +0300

    The FeatureAddCarCommandResponse has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/FeatureAddCar/FeatureAddCarCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Car/FeatureAddCar/FeatureAddCarCommandResponse.cs
@@ -1,8 +1,16 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.FeatureAddCar
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.FeatureAddCar
 {
 
     public class FeatureAddCarCommandResponse
     {
+        public Guid CarId { get; set; }
+        public Guid FeatureId { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
+
 
     }
 }
