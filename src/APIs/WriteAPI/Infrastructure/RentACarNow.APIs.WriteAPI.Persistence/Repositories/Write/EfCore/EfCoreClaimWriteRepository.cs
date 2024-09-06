72abf2ea320dae54838de968ed5fc21fbb45948d
Emircan <emircandv787@gmail.com>
2024-09-06 18:39:24 +0300

    The EfCoreClaimWriteRepository has been refactored.

--- src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreClaimWriteRepository.cs
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreClaimWriteRepository.cs
@@ -10,6 +10,23 @@ namespace RentACarNow.APIs.WriteAPI.Persistence.Repositories.Write.EfCore
         public EfCoreClaimWriteRepository(RentalACarNowDbContext context) : base(context)
         {
         }
+
+
+
+        public override Task UpdateAsync(Claim entity)
+        {
+            _table.Attach(entity);
+
+            var entityEntry = _table.Entry(entity);
+
+            entityEntry.Property(p => p.UpdatedDate).IsModified = true;
+            entityEntry.Property(p=>p.Key).IsModified = true;
+            entityEntry.Property(p=>p.Value).IsModified = true;
+
+            return Task.CompletedTask;
+        }
+
+
     }
 
 
