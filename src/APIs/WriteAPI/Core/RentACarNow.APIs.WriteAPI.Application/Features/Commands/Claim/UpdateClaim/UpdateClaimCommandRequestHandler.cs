043842535e2aeeda9762db2a63f8ed534956b945
Emircan <emircandv787@gmail.com>
2024-09-06 05:46:38 +0300

    The UpdateClaimCommandRequestHanler

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/UpdateClaim/UpdateClaimCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/UpdateClaim/UpdateClaimCommandRequestHandler.cs
@@ -2,12 +2,18 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.UpdateBrand;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.UpdateCar;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
+using RentACarNow.Common.Entities.OutboxEntities;
 using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.Claim;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Models;
+using System.Net;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateClaim
@@ -38,22 +44,52 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
 
         public async Task<UpdateClaimCommandResponse> Handle(UpdateClaimCommandRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogDebug($"{nameof(UpdateClaimCommandRequestHandler)} Handle method has been executed");
+
+
             var validationResult = await _validator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
-                return new UpdateClaimCommandResponse { };
+                _logger.LogInformation($"{nameof(UpdateClaimCommandRequestHandler)} Request not validated");
+
+                return new UpdateClaimCommandResponse
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = validationResult.Errors?.Select(vf => new ResponseErrorModel
+                    {
+                        PropertyName = vf.PropertyName,
+                        ErrorMessage = vf.ErrorMessage
+                    })
+                };
+
             }
 
-            var isExists = await _readRepository.IsExistsAsync(request.Id);
+            var isExists = await _readRepository.IsExistsAsync(request.ClaimId);
 
             if (!isExists)
-                return new UpdateClaimCommandResponse { };
+            {
+                _logger.LogInformation($"{nameof(UpdateClaimCommandRequestHandler)} Entity not found , id : {request.ClaimId}");
 
+                return new UpdateClaimCommandResponse
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "claim not found",
+                            PropertyName = null
+                        }
+                    }
+                };
+            }
 
             var claimEntity = _mapper.Map<EfEntity.Claim>(request);
+            claimEntity.UpdatedDate = DateHelper.GetDate();
 
             var claimUpdatedEvent = _mapper.Map<ClaimUpdatedEvent>(claimEntity);
+            claimUpdatedEvent.MessageId = Guid.NewGuid();
 
             using var efTran = await _writeRepository.BeginTransactionAsync();
             using var mongoSession = await _outboxRepository.StartSessionAsync();
@@ -67,18 +103,24 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
                 await _writeRepository.SaveChangesAsync();
 
 
-                await _outboxRepository.AddMessageAsync(new Common.Entities.OutboxEntities.ClaimOutboxMessage
+                var outboxMessage = new ClaimOutboxMessage
                 {
-                    AddedDate = DateTime.Now,
+                    AddedDate = DateHelper.GetDate(),
                     ClaimEventType = ClaimEventType.ClaimUpdatedEvent,
-                    Id = Guid.NewGuid(),
+                    Id = claimUpdatedEvent.MessageId,
                     Payload = claimUpdatedEvent.Serialize()!
+                    
 
+                };
 
-                }, mongoSession);
+                await _outboxRepository.AddMessageAsync(outboxMessage, mongoSession);
 
                 await mongoSession.CommitTransactionAsync();
                 await efTran.CommitAsync();
+
+
+                _logger.LogInformation($"{nameof(UpdateClaimCommandRequestHandler)} Transaction commited");
+
 
             }
             catch (Exception)
@@ -86,11 +128,30 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.UpdateCl
                 await efTran.RollbackAsync();
                 await mongoSession.AbortTransactionAsync();
 
-                throw;
+                _logger.LogError($"{nameof(UpdateCarCommandRequestHandler)} transaction rollbacked");
+
+                return new UpdateClaimCommandResponse
+                {
+
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "Transaction exception",
+                            PropertyName = null
+                        }
+                    }
+                };
             }
 
 
-            return new UpdateClaimCommandResponse { };
+
+            return new UpdateClaimCommandResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Errors = null
+            };
         }
     }
 
