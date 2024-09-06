bec7bd212bf9b5acedf61417723ec2d6d5f90574
Emircan <emircandv787@gmail.com>
2024-09-06 05:16:51 +0300

    DeleteClaimCommandRequestHandler has been refactored

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/DeleteClaim/DeleteClaimCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Claim/DeleteClaim/DeleteClaimCommandRequestHandler.cs
@@ -2,13 +2,19 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.CreateCar;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.DeleteCar;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.CreateClaim;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.Common.Entities.OutboxEntities;
 using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.Claim;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Models;
+using System.Net;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteClaim
@@ -40,23 +46,50 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteCl
 
         public async Task<DeleteClaimCommandResponse> Handle(DeleteClaimCommandRequest request, CancellationToken cancellationToken)
         {
+            _logger.LogDebug($"{nameof(DeleteClaimCommandRequestHandler)} Handle method has been executed");
+
+
             var validationResult = await _validator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
-                return new DeleteClaimCommandResponse();
+                _logger.LogInformation($"{nameof(DeleteClaimCommandRequestHandler)} Request not validated");
+
+
+                return new DeleteClaimCommandResponse
+                {
+                    
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = validationResult.Errors?.Select(vf => new ResponseErrorModel
+                    {
+                        PropertyName = vf.PropertyName,
+                        ErrorMessage = vf.ErrorMessage
+                    })
+                };
             }
 
-            var isExists = await _readRepository.IsExistsAsync(request.Id);
+            var isExists = await _readRepository.IsExistsAsync(request.ClaimId);
 
             if (!isExists)
             {
-                return new DeleteClaimCommandResponse();
+                _logger.LogInformation($"{nameof(DeleteClaimCommandRequestHandler)} claim not found , id : {request.ClaimId}");
+                return new DeleteClaimCommandResponse
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "claim not found",
+                            PropertyName = null
+                        }
+                    }
+                };
             }
-
             var claimEntity = _mapper.Map<EfEntity.Claim>(request);
 
             var claimDeletedEvent = _mapper.Map<ClaimDeletedEvent>(claimEntity);
+            claimDeletedEvent.DeletedDate = DateHelper.GetDate();
 
             using var efTran = _writeRepository.BeginTransaction();
             using var mongoSession = await _claimOutboxRepository.StartSessionAsync();
@@ -69,30 +102,50 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Claim.DeleteCl
                 _writeRepository.Delete(claimEntity);
                 _writeRepository.SaveChanges();
 
-                await _claimOutboxRepository.AddMessageAsync(new ClaimOutboxMessage
+
+                var outboxMessage = new ClaimOutboxMessage
                 {
-                    AddedDate = DateTime.UtcNow,
+                    Id = claimDeletedEvent.MessageId,
+                    AddedDate = DateHelper.GetDate(),
                     ClaimEventType = ClaimEventType.ClaimDeletedEvent,
-                    Id = Guid.NewGuid(),
                     Payload = claimDeletedEvent.Serialize()!
+                    
+                };
 
-                }, mongoSession);
-
-
-
+                await _claimOutboxRepository.AddMessageAsync(outboxMessage, mongoSession);
 
                 efTran.Commit();
                 await mongoSession.CommitTransactionAsync();
+
+                _logger.LogInformation($"{nameof(DeleteClaimCommandRequestHandler)} Transaction commited");
             }
             catch (Exception)
             {
                 await mongoSession.AbortTransactionAsync();
                 efTran.Rollback();
-                throw;
+
+                _logger.LogError($"{nameof(DeleteClaimCommandRequestHandler)} transaction rollbacked");
+
+                return new DeleteClaimCommandResponse
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "Transaction exception",
+                            PropertyName = null
+                        }
+                    }
+                };
             }
 
 
-            return new DeleteClaimCommandResponse();
+            return new DeleteClaimCommandResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Errors = null
+            };
         }
     }
 }
