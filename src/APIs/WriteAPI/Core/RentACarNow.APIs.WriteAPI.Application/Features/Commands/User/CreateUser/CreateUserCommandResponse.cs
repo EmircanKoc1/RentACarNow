9f1e7ce7f0f12789df2679e9d73742743fd19257
Emircan <emircandv787@gmail.com>
2024-09-08 01:20:41 +0300

    The CreateComamndResponse has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/CreateUser/CreateUserCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/CreateUser/CreateUserCommandResponse.cs
@@ -1,9 +1,14 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.CreateUser
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.CreateUser
 {
 
     public class CreateUserCommandResponse
     {
-        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
     }
 
 }
