c27397b2a7b7578910ce9c763ab1e41c882abeff
Emircan <emircandv787@gmail.com>
2024-09-09 02:21:15 +0300

    The ClaimAddUserCommandResponse has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimAddedUser/ClaimAddUserCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimAddedUser/ClaimAddUserCommandResponse.cs
@@ -1,8 +1,14 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAddedUser
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimAddedUser
 {
     public class ClaimAddUserCommandResponse
     {
-
+        public Guid UserId { get; set; }
+        public Guid ClaimId { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
     }
 
 }
