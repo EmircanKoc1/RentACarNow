72fecaea3bc1e29756981b08af271bf7820d3643
Emircan <emircandv787@gmail.com>
2024-09-08 23:37:33 +0300

    The UserMapProfile has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Mapping/UserMapProfile.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Mapping/UserMapProfile.cs
@@ -5,11 +5,6 @@ using RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUser;
 using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 using RentACarNow.Common.Events.Common.Messages;
 using RentACarNow.Common.Events.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Mapping
 {
@@ -21,12 +16,18 @@ namespace RentACarNow.APIs.WriteAPI.Application.Mapping
             CreateMap<User, UserCreatedEvent>();
             CreateMap<User, UserDeletedEvent>();
             CreateMap<User, UserUpdatedEvent>();
-            
+
 
 
             CreateMap<CreateUserCommandRequest, User>();
-            CreateMap<UpdateUserCommandRequest, User>();
-            CreateMap<DeleteUserCommandRequest, User>();
+
+
+            CreateMap<UpdateUserCommandRequest, User>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(p => p.UserId));
+
+            CreateMap<DeleteUserCommandRequest, User>()
+                 .ForMember(dest => dest.Id, src => src.MapFrom(p => p.UserId));
+
 
             CreateMap<User, UserMessage>()
                 .ReverseMap();
