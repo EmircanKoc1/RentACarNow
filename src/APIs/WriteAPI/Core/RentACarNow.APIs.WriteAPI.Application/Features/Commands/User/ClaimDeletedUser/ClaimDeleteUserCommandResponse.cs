1bb3eb6ef04df3d45c30c484fe3516ff7bf7f4ef
Emircan <emircandv787@gmail.com>
2024-09-09 02:22:02 +0300

    The ClaimDeleteUserCommandResponse has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimDeletedUser/ClaimDeleteUserCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/ClaimDeletedUser/ClaimDeleteUserCommandResponse.cs
@@ -1,8 +1,14 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimDeletedUser
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.ClaimDeletedUser
 {
     public class ClaimDeleteUserCommandResponse
     {
-
+        public Guid UserId { get; set; }
+        public Guid ClaimId { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
     }
 
 }
