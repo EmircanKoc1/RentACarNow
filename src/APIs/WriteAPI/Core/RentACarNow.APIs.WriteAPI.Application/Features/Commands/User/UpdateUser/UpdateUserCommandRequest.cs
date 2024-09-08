9b09d27d1969180601438cd41ae2824ac4fb714f
Emircan <emircandv787@gmail.com>
2024-09-08 23:37:53 +0300

    The UpdateUserCommandRequest has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandRequest.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandRequest.cs
@@ -9,16 +9,14 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUse
 {
     public class UpdateUserCommandRequest : IRequest<UpdateUserCommandResponse>
     {
-        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
         public string PhoneNumber { get; set; }
-        //public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        //public decimal WalletBalance { get; set; }
+        public decimal WalletBalance { get; set; }
     }
 
 }
