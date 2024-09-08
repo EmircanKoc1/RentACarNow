dc47900854d5b1d21498b23ab22a1d1c9fed8ada
Emircan <emircandv787@gmail.com>
2024-09-08 20:54:19 +0300

    The UpdateUserCommandrResponse has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandResponse.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandResponse.cs
@@ -1,7 +1,13 @@
-﻿namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUser
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUser
 {
     public class UpdateUserCommandResponse
     {
+        public HttpStatusCode StatusCode { get; set; }
+        public IEnumerable<ResponseErrorModel>? Errors { get; set; }
+
 
     }
 
