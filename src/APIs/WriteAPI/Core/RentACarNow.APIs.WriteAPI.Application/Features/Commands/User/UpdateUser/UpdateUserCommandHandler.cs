c39edbbf7736e12cbe8d6d74134b288a42ded09d
Emircan <emircandv787@gmail.com>
2024-09-08 23:38:10 +0300

    The UpdateUserCommandHandler has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/User/UpdateUser/UpdateUserCommandHandler.cs
@@ -2,32 +2,37 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Brand.UpdateBrand;
+using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.UpdateCar;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Read.EfCore;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.Common.Entities.OutboxEntities;
 using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
 using RentACarNow.Common.Events.User;
 using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Models;
+using System.Net;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 
 namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUser
 {
-    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommandRequest, UpdateUserCommandResponse>
+    public class UpdateUserCommandRequestHandler : IRequestHandler<UpdateUserCommandRequest, UpdateUserCommandResponse>
     {
 
         private readonly IEfCoreUserWriteRepository _userWriteRepository;
         private readonly IEfCoreUserReadRepository _userReadRepository;
         private readonly IUserOutboxRepository _userOutboxRepository;
-        private readonly ILogger<UpdateUserCommandHandler> _logger;
+        private readonly ILogger<UpdateUserCommandRequestHandler> _logger;
         private readonly IValidator<UpdateUserCommandRequest> _validator;
         private readonly IMapper _mapper;
 
-        public UpdateUserCommandHandler(
+        public UpdateUserCommandRequestHandler(
             IEfCoreUserWriteRepository userWriteRepository,
             IEfCoreUserReadRepository userReadRepository,
             IUserOutboxRepository userOutboxRepository,
-            ILogger<UpdateUserCommandHandler> logger,
+            ILogger<UpdateUserCommandRequestHandler> logger,
             IValidator<UpdateUserCommandRequest> validator,
             IMapper mapper)
         {
@@ -42,15 +47,51 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUse
         public async Task<UpdateUserCommandResponse> Handle(UpdateUserCommandRequest request, CancellationToken cancellationToken)
         {
 
+            _logger.LogDebug($"{nameof(UpdateUserCommandRequestHandler)} Handle method has been executed");
+
+
             var validationResult = await _validator.ValidateAsync(request);
 
             if (!validationResult.IsValid)
             {
-                return new UpdateUserCommandResponse();
+                _logger.LogInformation($"{nameof(UpdateUserCommandRequestHandler)} Request not validated");
+
+                return new UpdateUserCommandResponse
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = validationResult.Errors?.Select(vf => new ResponseErrorModel
+                    {
+                        PropertyName = vf.PropertyName,
+                        ErrorMessage = vf.ErrorMessage
+                    })
+                };
+
+            }
+            var userExists = await _userReadRepository.IsExistsAsync(request.UserId);
+
+            if (!userExists)
+            {
+                _logger.LogInformation($"{nameof(UpdateUserCommandRequestHandler)} Entity not found , id : {request.UserId}");
+
+                return new UpdateUserCommandResponse
+                {
+                    StatusCode = HttpStatusCode.NotFound,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "user not found",
+                            PropertyName = null
+                        }
+}
+                };
+
+
             }
 
-            var efEntity = _mapper.Map<EfEntity.User>(request);
 
+            var efEntity = _mapper.Map<EfEntity.User>(request);
+            efEntity.UpdatedDate = DateHelper.GetDate();
 
             var userUpdatedEvent = _mapper.Map<UserUpdatedEvent>(efEntity);
 
@@ -65,27 +106,52 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.User.UpdateUse
 
                 await _userWriteRepository.UpdateAsync(efEntity);
                 await _userWriteRepository.SaveChangesAsync();
-                await _userOutboxRepository.AddMessageAsync(new UserOutboxMessage
+
+                var outboxMessage = new UserOutboxMessage
                 {
-                    AddedDate = DateTime.Now,
+                    AddedDate = DateHelper.GetDate(),
                     EventType = UserEventType.UserUpdatedEvent,
                     Payload = userUpdatedEvent.Serialize()!
-                }, mongoSession);
+                };
 
-
+                await _userOutboxRepository.AddMessageAsync(outboxMessage, mongoSession);
 
                 await mongoSession.CommitTransactionAsync();
                 await efTran.CommitAsync();
+
+                _logger.LogInformation($"{nameof(UpdateUserCommandRequestHandler)} Transaction commited");
+
             }
             catch (Exception)
             {
 
                 await mongoSession.AbortTransactionAsync();
                 await efTran.CommitAsync();
-                throw;
+
+                _logger.LogError($"{nameof(UpdateUserCommandRequestHandler)} transaction rollbacked");
+
+                return new UpdateUserCommandResponse
+                {
+
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Errors = new List<ResponseErrorModel>(capacity: 1)
+                    {
+                        new ResponseErrorModel
+                        {
+                            ErrorMessage = "Transaction exception",
+                            PropertyName = null
+                        }
+                    }
+                };
+
+
             }
 
-            return null;
+            return new UpdateUserCommandResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Errors = null
+            };
 
 
 
