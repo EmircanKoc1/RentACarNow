4b40f57d0055de9cf86223437e05a1547994f356
Emircan <emircandv787@gmail.com>
2024-09-09 00:01:24 +0300

    The EfCoreUserWriteRepository has been refactored.

--- src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreUserWriteRepository.cs
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreUserWriteRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using RentACarNow.APIs.WriteAPI.Application.Repositories.Write.EfCore;
 using RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
 using RentACarNow.APIs.WriteAPI.Persistence.Contexts;
@@ -32,5 +33,25 @@ namespace RentACarNow.APIs.WriteAPI.Persistence.Repositories.Write.EfCore
 
             user.Claims.Remove(claim);
         }
+
+
+        public override Task UpdateAsync(User entity)
+        {
+            _table.Attach(entity);
+
+            var entityEntry = _table.Entry(entity);
+
+            entityEntry.Property(u => u.Age).IsModified = true;
+            entityEntry.Property(u => u.Email).IsModified = true;
+            entityEntry.Property(u => u.Password).IsModified = true;
+            entityEntry.Property(u => u.PhoneNumber).IsModified = true;
+            entityEntry.Property(u => u.Surname).IsModified = true;
+            entityEntry.Property(u => u.Name).IsModified = true;
+            entityEntry.Property(u => u.WalletBalance).IsModified = true;
+
+            return Task.CompletedTask;
+
+        }
+
     }
 }
