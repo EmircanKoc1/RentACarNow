37172867278c0545c74215615110c67f8f6052e6
Emircan <emircandv787@gmail.com>
2024-09-09 18:31:19 +0300

    The new migration has been created and update database .

--- src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Migrations/20240909151025_mig_4.Designer.cs
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Migrations/20240909151025_mig_4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentACarNow.APIs.WriteAPI.Persistence.Contexts;
 
@@ -11,9 +12,11 @@ using RentACarNow.APIs.WriteAPI.Persistence.Contexts;
 namespace RentACarNow.APIs.WriteAPI.Persistence.Migrations
 {
     [DbContext(typeof(RentalACarNowDbContext))]
-    partial class RentalACarNowDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240909151025_mig_4")]
+    partial class mig_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Migrations/20240909151025_mig_4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentACarNow.APIs.WriteAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRental",
+                table: "Cars",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsRental",
+                table: "Cars");
+        }
+    }
+}
