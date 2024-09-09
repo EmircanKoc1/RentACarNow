d3d31efe2f5884b98e9c8461bd34bbd15bc19ed9
Emircan <emircandv787@gmail.com>
2024-09-09 22:23:39 +0300

    The EfCoreCarWriteRepository has been refactored.

--- src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreCarWriteRepository.cs
+++ src/APIs/WriteAPI/Infrastructure/RentACarNow.APIs.WriteAPI.Persistence/Repositories/Write/EfCore/EfCoreCarWriteRepository.cs
@@ -11,16 +11,32 @@ namespace RentACarNow.APIs.WriteAPI.Persistence.Repositories.Write.EfCore
         {
         }
 
-        //public async override Task UpdateAsync(Car entity)
-        //{
-        //    _context.Cars.Attach(entity);
+        public override Task UpdateAsync(Car entity)
+        {
+            _table.Attach(entity);
 
+            var entityEntry = _table.Entry(entity);
 
+            entityEntry.Property(c => c.Name).IsModified = true;
+            entityEntry.Property(c => c.Modal).IsModified = true;
+            entityEntry.Property(c => c.Title).IsModified = true;
+            entityEntry.Property(c => c.HourlyRentalPrice).IsModified = true;
+            entityEntry.Property(c => c.Kilometer).IsModified = true;
+            entityEntry.Property(c => c.Description).IsModified = true;
+            entityEntry.Property(c => c.Color).IsModified = true;
+            entityEntry.Property(c => c.PassengerCapacity).IsModified = true;
+            entityEntry.Property(c => c.LuggageCapacity).IsModified = true;
+            entityEntry.Property(c => c.FuelConsumption).IsModified = true;
+            entityEntry.Property(c => c.CarFuelType).IsModified = true;
+            entityEntry.Property(c => c.TransmissionType).IsModified = true;
+            entityEntry.Property(c => c.ReleaseDate).IsModified = true;
+            entityEntry.Property(c => c.IsRental).IsModified = true;
+            entityEntry.Property(c => c.BrandId).IsModified = true;
+            entityEntry.Property(c => c.UpdatedDate).IsModified = true;
 
+            return Task.CompletedTask;
 
-
-        //}
-
+        }
 
     }
 }
