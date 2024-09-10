d694156d2f2c81847ffe29d861ba3e06b258a554
Emircan <emircandv787@gmail.com>
2024-09-10 23:09:31 +0300

    The RentalInboxMessage has been defined.

---
+++ src/Common/RentACarNow.Common.Entities/InboxEntities/RentalInboxMessage.cs
@@ -0,0 +1,10 @@
+﻿using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+
+namespace RentACarNow.Common.Entities.InboxEntities
+{
+    public class RentalInboxMessage : BaseInboxMessage
+    {
+        public RentalEventType EventType { get; set; }
+
+    }
+}
