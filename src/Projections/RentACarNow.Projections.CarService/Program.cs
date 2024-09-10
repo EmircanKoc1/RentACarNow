e1cd45511388e7f9f1ce87429ea449c85d72c4c5
Emircan <emircandv787@gmail.com>
2024-09-10 21:08:30 +0300

    The Projections.CarService  program.cs has been refactored.

--- src/Projections/RentACarNow.Projections.CarService/Program.cs
+++ src/Projections/RentACarNow.Projections.CarService/Program.cs
@@ -1,7 +1,34 @@
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.InboxContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
 using RentACarNow.Projections.CarService;
-
+using RentACarNow.Projections.CarService.Consumers;
+using RentACarNow.Common.Infrastructure.Services;
+using RentACarNow.Common.Infrastructure.Extensions;
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+
+builder.Services.AddHostedService<ProjectionService>();
+builder.Services.AddHostedService<CarCreatedEventConsumer>();
+builder.Services.AddHostedService<CarDeletedEventConsumer>();
+builder.Services.AddHostedService<CarUpdatedEventConsumer>();
+builder.Services.AddHostedService<CarFeatureAddedEventConsumer>();
+builder.Services.AddHostedService<CarFeatureDeletedEventConsumer>();
+builder.Services.AddHostedService<CarFeatureUpdatedEventConsumer>();
+
+
+builder.Services.AddSingleton<MongoCarInboxContext>(p =>
+{
+    return new MongoCarInboxContext(
+    mongoClient: new MongoClient(MongoDbConstants.CONNECTION_STRING),
+    databaseName: "InboxDB");
+});
+
+builder.Services.AddSingleton<IBrandInboxRepository, BrandInboxMongoRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "CarProjectionService");
 
 var host = builder.Build();
 host.Run();
