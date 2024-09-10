e1b6bf970c25a21402e4768b0153a8e24aa8ad54
Emircan <emircandv787@gmail.com>
2024-09-10 17:44:29 +0300

    The CarFeatureAddedEventConsumer has been added.

---
+++ src/Projections/RentACarNow.Projections.CarService/Consumers/CarFeatureAddedEventConsumer.cs
@@ -0,0 +1,58 @@
+﻿using RentACarNow.Common.Constants.MessageBrokers.Queues;
+using RentACarNow.Common.Entities.InboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Infrastructure.Helpers;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Infrastructure.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RentACarNow.Common.Events.Car;
+
+namespace RentACarNow.Projections.CarService.Consumers
+{
+    internal class CarFeatureAddedEventConsumer : BackgroundService
+    {
+        private readonly ICarInboxRepository _inboxRepository;
+        private readonly ILogger<CarFeatureAddedEventConsumer> _logger;
+        private readonly IRabbitMQMessageService _messageService;
+
+        public CarFeatureAddedEventConsumer(ICarInboxRepository inboxRepository, ILogger<CarFeatureAddedEventConsumer> logger, IRabbitMQMessageService messageService)
+        {
+            _inboxRepository = inboxRepository;
+            _logger = logger;
+            _messageService = messageService;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"{nameof(CarFeatureAddedEventConsumer)} execute method has been executed");
+
+            _messageService.ConsumeQueue(
+               queueName: RabbitMQQueues.CAR_FEATURE_ADDED_QUEUE,
+               consumeOperations: async (message) =>
+               {
+                   _logger.LogInformation(message);
+
+                   var @event = message.Deseralize<CarFeatureAddedEvent>();
+
+                   var foundedInboxMessage = await _inboxRepository.GetMessageByIdAsync(@event.MessageId);
+
+                   if (foundedInboxMessage is not null) return;
+
+                   var inboxMessage = new CarInboxMessage
+                   {
+                       AddedDate = DateHelper.GetDate(),
+                       EventType = CarEventType.CarFeatureAddedEvent,
+                       MessageId = @event.MessageId,
+                       Payload = message
+                   };
+
+                   await _inboxRepository.AddMessageAsync(inboxMessage);
+               });
+        }
+    }
+}
