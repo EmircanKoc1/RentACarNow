9759db9f357d023b9d29f27d36f10fe67e3c69ad
Emircan <emircandv787@gmail.com>
2024-09-10 22:01:52 +0300

    The ClaimDeletedEventConsumer has been completed.

---
+++ src/Projections/RentACarNow.Projections.ClaimService/Consumers/ClaimDeletedEventConsumer.cs
@@ -0,0 +1,56 @@
+﻿using RentACarNow.Common.Constants.MessageBrokers.Queues;
+using RentACarNow.Common.Entities.InboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Events.Claim;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+
+namespace RentACarNow.Projections.ClaimService.Consumers
+{
+    public class ClaimDeletedEventConsumer : BackgroundService
+    {
+        private readonly IClaimnboxRepository _claimInboxRepository;
+        private readonly ILogger<ClaimDeletedEventConsumer> _logger;
+        private readonly IRabbitMQMessageService _messageService;
+
+        public ClaimDeletedEventConsumer(
+            IClaimnboxRepository claimInboxRepository,
+            ILogger<ClaimDeletedEventConsumer> logger,
+            IRabbitMQMessageService messageService)
+        {
+            _claimInboxRepository = claimInboxRepository;
+            _logger = logger;
+            _messageService = messageService;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"{nameof(ClaimDeletedEventConsumer)} execute method has been executed");
+
+            _messageService.ConsumeQueue(
+                queueName: RabbitMQQueues.CLAIM_DELETED_QUEUE,
+                consumeOperations: async (message) =>
+                {
+                    _logger.LogInformation(message);
+
+                    var @event = message.Deseralize<ClaimDeletedEvent>();
+
+                    var foundedInboxMessage = await _claimInboxRepository.GetMessageByIdAsync(@event.MessageId);
+
+                    if (foundedInboxMessage is not null) return;
+
+                    var inboxMessage = new ClaimInboxMessage
+                    {
+                        AddedDate = DateHelper.GetDate(),
+                        EventType = ClaimEventType.ClaimDeletedEvent,
+                        MessageId = @event.MessageId,
+                        Payload = message
+                    };
+
+                    await _claimInboxRepository.AddMessageAsync(inboxMessage);
+                });
+        }
+    }
+}
