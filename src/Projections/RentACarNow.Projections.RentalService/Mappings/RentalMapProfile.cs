9c8c88732bd406421181ea3888cdc677ad8ecabd
Emircan <emircandv787@gmail.com>
2024-09-10 23:38:45 +0300

    The RentalMapProfile has been defined.

---
+++ src/Projections/RentACarNow.Projections.RentalService/Mappings/RentalMapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Events.Rental;
+using RentACarNow.Common.MongoEntities;
+
+namespace RentACarNow.Projections.RentalService.Mappings
+{
+    internal class RentalMapProfile : Profile
+    {
+
+        public RentalMapProfile()
+        {
+            CreateMap<RentalCreatedEvent, Rental>();
+            CreateMap<RentalUpdatedEvent, Rental>();
+            CreateMap<RentalDeletedEvent, Rental>();
+
+
+        }
+
+    }
+}
