7b9649b4e35b38509413cf0c3798852e14a6448c
Emircan <emircandv787@gmail.com>
2024-09-10 22:44:39 +0300

    The ClaimMapProfile has been added.

---
+++ src/Projections/RentACarNow.Projections.ClaimService/Mappings/ClaimMapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Events.Claim;
+using RentACarNow.Common.MongoEntities;
+
+namespace RentACarNow.Projections.ClaimService.Mappings
+{
+    internal class ClaimMapProfile : Profile
+    {
+
+        public ClaimMapProfile()
+        {
+            CreateMap<ClaimAddedEvent, Claim>();
+            CreateMap<ClaimUpdatedEvent, Claim>();
+            CreateMap<ClaimDeletedEvent, Claim>();
+        }
+
+    }
+}
