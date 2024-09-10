427ad3693c1143729db4b656a1c0992ea68aea36
Emircan <emircandv787@gmail.com>
2024-09-10 22:03:49 +0300

    The Program.cs has been refactored.

--- src/Projections/RentACarNow.Projections.ClaimService/Program.cs
+++ src/Projections/RentACarNow.Projections.ClaimService/Program.cs
@@ -1,7 +1,43 @@
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.InboxContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Write.Mongo;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
 using RentACarNow.Projections.ClaimService;
+using RentACarNow.Projections.ClaimService.Consumers;
+using System.Reflection;
+using RentACarNow.Common.Infrastructure.Extensions;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+
+builder.Services.AddHostedService<ProjectionService>();
+
+
+builder.Services.AddHostedService<ClaimDeletedEventConsumer>();
+builder.Services.AddHostedService<ClaimAddedEventConsumer>();
+builder.Services.AddHostedService<ClaimUpdatedEventConsumer>();
+
+builder.Services.AddSingleton<MongoClaimInboxContext>(p =>
+{
+    return new MongoClaimInboxContext(
+    mongoClient: new MongoClient(MongoDbConstants.CONNECTION_STRING),
+    databaseName: "InboxDB");
+});
+
+builder.Services.AddSingleton<IClaimnboxRepository, ClaimInboxMongoRepository>();
+
+
+builder.Services.AddMongoRentalACarNowDBContext();
+builder.Services.AddSingleton<IMongoClaimWriteRepository, MongoClaimWriteRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "ClaimProjectionService");
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
 
 var host = builder.Build();
 host.Run();
