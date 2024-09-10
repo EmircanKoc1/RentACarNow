148d0ae6143336628212fafe6b18a380f491733c
Emircan <emircandv787@gmail.com>
2024-09-10 21:28:44 +0300

    The CarMapProfile has been added .

---
+++ src/Projections/RentACarNow.Projections.CarService/Mappings/CarMapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Events.Car;
+using RentACarNow.Common.MongoEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Projections.CarService.Mappings
+{
+    internal class CarMapProfile : Profile
+    {
+        public CarMapProfile()
+        {
+            CreateMap<CarCreatedEvent, Car>();
+            CreateMap<CarDeletedEvent, Car>();
+            CreateMap<CarUpdatedEvent, Car>();
+            
+        }
+    }
+}
