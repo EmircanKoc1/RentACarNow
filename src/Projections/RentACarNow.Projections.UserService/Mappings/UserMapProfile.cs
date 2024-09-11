a116f43f60d41acccd26e48888e7b1015050c45e
Emircan <emircandv787@gmail.com>
2024-09-11 18:40:27 +0300

    The UserMapProfile has been defined.

---
+++ src/Projections/RentACarNow.Projections.UserService/Mappings/UserMapProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Events.User;
+using System.Security.Claims;
+
+namespace RentACarNow.Projections.UserService.Mappings
+{
+    internal class UserMapProfile : Profile
+    {
+        public UserMapProfile()
+        {
+            CreateMap<UserCreatedEvent, User>();
+            CreateMap<UserDeletedEvent, User>();
+            CreateMap<UserUpdatedEvent, User>();
+            CreateMap<UserClaimDeletedEvent, Claim>();
+            CreateMap<UserClaimUpdatedEvent, Claim>();
+            CreateMap<UserClaimUpdatedEvent, Claim>();
+        }
+
+    }
+}
