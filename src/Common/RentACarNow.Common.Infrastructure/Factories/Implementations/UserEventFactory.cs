eba5983e89a355484833714e53334b7cd81b3fd8
Emircan <emircandv787@gmail.com>
2024-09-12 02:31:18 +0300

    The IUserEventFactory and UserEventFactory have been completed.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Implementations/UserEventFactory.cs
@@ -0,0 +1,86 @@
+﻿using RentACarNow.Common.Events.Common.Messages;
+using RentACarNow.Common.Events.User;
+using RentACarNow.Common.Infrastructure.Factories.Interfaces;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Implementations
+{
+    public class UserEventFactory : IUserEventFactory
+    {
+        public UserCreatedEvent CreateUserCreatedEvent(
+            Guid userId,
+            string name,
+            string surname,
+            int age,
+            string phoneNumber,
+            string username,
+            string email,
+            string password,
+            decimal walletBalance,
+            DateTime createdDate)
+        {
+            return new UserCreatedEvent(
+                userId,
+                name,
+                surname,
+                age,
+                phoneNumber,
+                username,
+                email,
+                password,
+                walletBalance,
+                createdDate);
+        }
+
+        public UserUpdatedEvent CreateUserUpdatedEvent(
+            Guid userId,
+            string name,
+            string surname,
+            int age,
+            string phoneNumber,
+            string username,
+            string email,
+            string password,
+            decimal walletBalance,
+            DateTime updatedDate)
+        {
+            return new UserUpdatedEvent(
+                userId,
+                name,
+                surname,
+                age,
+                phoneNumber,
+                username,
+                email,
+                password,
+                walletBalance,
+                updatedDate);
+        }
+
+        public UserDeletedEvent CreateUserDeletedEvent(Guid userId, DateTime deletedDate)
+        {
+            return new UserDeletedEvent(userId, deletedDate);
+        }
+
+        public UserClaimAddedEvent CreateUserClaimAddedEvent(Guid userId, Guid claimId, string key, string value)
+        {
+            var claimMessage = new ClaimMessage
+            {
+                ClaimId = claimId,
+                Key = key,
+                Value = value
+            };
+
+            return new UserClaimAddedEvent(userId, claimMessage);
+        }
+
+        public UserClaimUpdatedEvent CreateUserClaimUpdatedEvent(Guid claimId, string key, string value)
+        {
+            return new UserClaimUpdatedEvent(claimId, key, value);
+        }
+
+        public UserClaimDeletedEvent CreateUserClaimDeletedEvent(Guid userId, Guid claimId)
+        {
+            return new UserClaimDeletedEvent(userId, claimId);
+        }
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Interfaces/IUserEventFactory.cs
@@ -0,0 +1,36 @@
+﻿using RentACarNow.Common.Events.User;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Interfaces
+{
+    public interface IUserEventFactory
+    {
+        UserCreatedEvent CreateUserCreatedEvent(
+            Guid userId,
+            string name,
+            string surname,
+            int age,
+            string phoneNumber,
+            string username,
+            string email,
+            string password,
+            decimal walletBalance,
+            DateTime createdDate);
+
+        UserUpdatedEvent CreateUserUpdatedEvent(
+            Guid userId,
+            string name,
+            string surname,
+            int age,
+            string phoneNumber,
+            string username,
+            string email,
+            string password,
+            decimal walletBalance,
+            DateTime updatedDate);
+
+        UserDeletedEvent CreateUserDeletedEvent(Guid userId, DateTime deletedDate);
+        UserClaimAddedEvent CreateUserClaimAddedEvent(Guid userId, Guid claimId, string key, string value);
+        UserClaimUpdatedEvent CreateUserClaimUpdatedEvent(Guid claimId, string key, string value);
+        UserClaimDeletedEvent CreateUserClaimDeletedEvent(Guid userId, Guid claimId);
+    }
+}
