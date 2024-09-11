e43b41a7bd78b0f7ed09cf84315399bb050f209c
Emircan <emircandv787@gmail.com>
2024-09-12 00:43:39 +0300

    The IClaimEventFactory and ClaimEventFactory have been completed.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Implementations/ClaimEventFactory.cs
@@ -0,0 +1,24 @@
+﻿using RentACarNow.Common.Events.Claim;
+using RentACarNow.Common.Infrastructure.Factories.Interfaces;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Implementations
+{
+    public class ClaimEventFactory : IClaimEventFactory
+    {
+        public ClaimCreatedEvent CreateClaimCreatedEvent(Guid claimId, string key, string value, DateTime createdDate)
+        {
+            return new ClaimCreatedEvent(claimId, key, value, createdDate);
+        }
+
+        public ClaimDeletedEvent CreateClaimDeletedEvent(Guid claimId, DateTime deletedDate)
+        {
+            return new ClaimDeletedEvent(claimId, deletedDate);
+        }
+
+        public ClaimUpdatedEvent CreateClaimUpdatedEvent(Guid claimId, string key, string value, DateTime updatedDate)
+        {
+            return new ClaimUpdatedEvent(claimId, key, value, updatedDate);
+
+        }
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Interfaces/IClaimEventFactory.cs
@@ -0,0 +1,13 @@
+﻿using RentACarNow.Common.Events.Claim;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Interfaces
+{
+    public interface IClaimEventFactory
+    {
+
+        ClaimCreatedEvent CreateClaimCreatedEvent(Guid claimId, string key, string value, DateTime createdDate);
+        ClaimUpdatedEvent CreateClaimUpdatedEvent(Guid claimId, string key, string value, DateTime updatedDate);
+
+        ClaimDeletedEvent CreateClaimDeletedEvent(Guid claimId, DateTime deletedDate);
+    }
+}
