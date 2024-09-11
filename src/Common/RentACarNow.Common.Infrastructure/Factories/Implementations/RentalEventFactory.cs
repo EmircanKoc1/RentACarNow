95028d953049ef2923a967c7048be28f73a344e8
Emircan <emircandv787@gmail.com>
2024-09-12 01:48:01 +0300

    The IRentalEventFactory and RentalEventFactory have been completed.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Implementations/RentalEventFactory.cs
@@ -0,0 +1,156 @@
+﻿using RentACarNow.Common.Enums.EntityEnums;
+using RentACarNow.Common.Events.Common.Messages;
+using RentACarNow.Common.Events.Rental;
+using RentACarNow.Common.Infrastructure.Factories.Interfaces;
+using System;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Implementations
+{
+    public class RentalEventFactory : IRentalEventFactory
+    {
+        public RentalCreatedEvent CreateRentalCreatedEvent(
+            Guid rentalId,
+            DateTime rentalStartedDate,
+            DateTime rentalEndDate,
+            decimal hourlyRentalPrice,
+            decimal totalRentalPrice,
+            Guid carId,
+            string carName,
+            string carModal,
+            string carTitle,
+            decimal carHourlyRentalPrice,
+            float carKilometer,
+            string carDescription,
+            string carColor,
+            int carPassengerCapacity,
+            int carLuggageCapacity,
+            float carFuelConsumption,
+            DateTime? carReleaseDate,
+            FuelType carFuelType,
+            TransmissionType carTransmissionType,
+            Guid userId,
+            string userName,
+            string userSurname,
+            int userAge,
+            string userPhoneNumber,
+            string userEmail,
+            RentalStatus status,
+            DateTime createdDate)
+        {
+            var carMessage = new CarMessage
+            {
+                CarId = carId,
+                Name = carName,
+                Modal = carModal,
+                Title = carTitle,
+                HourlyRentalPrice = carHourlyRentalPrice,
+                Kilometer = carKilometer,
+                Description = carDescription,
+                Color = carColor,
+                PassengerCapacity = carPassengerCapacity,
+                LuggageCapacity = carLuggageCapacity,
+                FuelConsumption = carFuelConsumption,
+                ReleaseDate = carReleaseDate,
+                CarFuelType = carFuelType,
+                TransmissionType = carTransmissionType
+            };
+
+            var userMessage = new UserMessage
+            {
+                UserId = userId,
+                Name = userName,
+                Surname = userSurname,
+                Age = userAge,
+                PhoneNumber = userPhoneNumber,
+                Email = userEmail
+            };
+
+            return new RentalCreatedEvent(
+                rentalId,
+                rentalStartedDate,
+                rentalEndDate,
+                hourlyRentalPrice,
+                totalRentalPrice,
+                carMessage,
+                userMessage,
+                status,
+                createdDate
+            );
+        }
+
+        public RentalDeletedEvent CreateRentalDeletedEvent(Guid rentalId, DateTime deletedDate)
+        {
+            return new RentalDeletedEvent(rentalId, deletedDate);
+        }
+
+        public RentalUpdatedEvent CreateRentalUpdatedEvent(
+            Guid rentalId,
+            DateTime rentalStartedDate,
+            DateTime rentalEndDate,
+            decimal hourlyRentalPrice,
+            decimal totalRentalPrice,
+            Guid carId,
+            string carName,
+            string carModal,
+            string carTitle,
+            decimal carHourlyRentalPrice,
+            float carKilometer,
+            string carDescription,
+            string carColor,
+            int carPassengerCapacity,
+            int carLuggageCapacity,
+            float carFuelConsumption,
+            DateTime? carReleaseDate,
+            FuelType carFuelType,
+            TransmissionType carTransmissionType,
+            Guid userId,
+            string userName,
+            string userSurname,
+            int userAge,
+            string userPhoneNumber,
+            string userEmail,
+            RentalStatus status,
+            DateTime updatedDate)
+        {
+            var carMessage = new CarMessage
+            {
+                CarId = carId,
+                Name = carName,
+                Modal = carModal,
+                Title = carTitle,
+                HourlyRentalPrice = carHourlyRentalPrice,
+                Kilometer = carKilometer,
+                Description = carDescription,
+                Color = carColor,
+                PassengerCapacity = carPassengerCapacity,
+                LuggageCapacity = carLuggageCapacity,
+                FuelConsumption = carFuelConsumption,
+                ReleaseDate = carReleaseDate,
+                CarFuelType = carFuelType,
+                TransmissionType = carTransmissionType
+            };
+
+            var userMessage = new UserMessage
+            {
+                UserId = userId,
+                Name = userName,
+                Surname = userSurname,
+                Age = userAge,
+                PhoneNumber = userPhoneNumber,
+                Email = userEmail
+            };
+
+            return new RentalUpdatedEvent(
+                rentalId,
+                rentalStartedDate,
+                rentalEndDate,
+                hourlyRentalPrice,
+                totalRentalPrice,
+                carMessage,
+                userMessage,
+                status,
+                updatedDate
+            );
+        }
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Interfaces/IRentalEventFactory.cs
@@ -0,0 +1,71 @@
+﻿using RentACarNow.Common.Events.Rental;
+using RentACarNow.Common.Enums.EntityEnums;
+using System;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Interfaces
+{
+    public interface IRentalEventFactory
+    {
+        RentalCreatedEvent CreateRentalCreatedEvent(
+            Guid rentalId,
+            DateTime rentalStartedDate,
+            DateTime rentalEndDate,
+            decimal hourlyRentalPrice,
+            decimal totalRentalPrice,
+            Guid carId,
+            string carName,
+            string carModal,
+            string carTitle,
+            decimal carHourlyRentalPrice,
+            float carKilometer,
+            string carDescription,
+            string carColor,
+            int carPassengerCapacity,
+            int carLuggageCapacity,
+            float carFuelConsumption,
+            DateTime? carReleaseDate,
+            FuelType carFuelType,
+            TransmissionType carTransmissionType,
+            Guid userId,
+            string userName,
+            string userSurname,
+            int userAge,
+            string userPhoneNumber,
+            string userEmail,
+            RentalStatus status,
+            DateTime createdDate);
+
+        RentalUpdatedEvent CreateRentalUpdatedEvent(
+            Guid rentalId,
+            DateTime rentalStartedDate,
+            DateTime rentalEndDate,
+            decimal hourlyRentalPrice,
+            decimal totalRentalPrice,
+            Guid carId,
+            string carName,
+            string carModal,
+            string carTitle,
+            decimal carHourlyRentalPrice,
+            float carKilometer,
+            string carDescription,
+            string carColor,
+            int carPassengerCapacity,
+            int carLuggageCapacity,
+            float carFuelConsumption,
+            DateTime? carReleaseDate,
+            FuelType carFuelType,
+            TransmissionType carTransmissionType,
+            Guid userId,
+            string userName,
+            string userSurname,
+            int userAge,
+            string userPhoneNumber,
+            string userEmail,
+            RentalStatus status,
+            DateTime updatedDate);
+
+        RentalDeletedEvent CreateRentalDeletedEvent(
+            Guid rentalId,
+            DateTime deletedDate);
+    }
+}
