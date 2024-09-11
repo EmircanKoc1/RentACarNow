28bc1623e6eea95aa88c6c3f4b5460dd1826c8c5
Emircan <emircandv787@gmail.com>
2024-09-11 18:34:20 +0300

    The UserClaimAddedEventConsumer has been completed.

---
+++ src/Projections/RentACarNow.Projections.UserService/Consumers/UserClaimAddedEventConsumer.cs
@@ -0,0 +1,62 @@
+﻿using RentACarNow.Common.Constants.MessageBrokers.Queues;
+using RentACarNow.Common.Entities.InboxEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces;
+using RentACarNow.Common.MongoEntities;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using RentACarNow.Common.Events.User;
+
+namespace RentACarNow.Projections.UserClaimService.Consumers
+{
+    public class UserClaimAddedEventConsumer : BackgroundService
+    {
+        private readonly IUserInboxRepository _inboxRepository;
+        private readonly ILogger<UserClaimAddedEventConsumer> _logger;
+        private readonly IRabbitMQMessageService _messageService;
+
+        public UserClaimAddedEventConsumer(
+            IUserInboxRepository inboxRepository,
+            ILogger<UserClaimAddedEventConsumer> logger,
+            IRabbitMQMessageService messageService)
+        {
+            _inboxRepository = inboxRepository;
+            _logger = logger;
+            _messageService = messageService;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"{nameof(UserClaimAddedEventConsumer)} execute method has been executed");
+
+            _messageService.ConsumeQueue(
+                queueName: RabbitMQQueues.USER_CLAIM_ADDED_QUEUE,
+                consumeOperations: async (message) =>
+                {
+                    _logger.LogInformation(message);
+
+                    var @event = message.Deseralize<UserClaimAddedEvent>();
+
+                    var foundedInboxMessage = await _inboxRepository.GetMessageByIdAsync(@event.MessageId);
+
+                    if (foundedInboxMessage is not null) return;
+
+                    var inboxMessage = new UserInboxMessage
+                    {
+                        AddedDate = DateHelper.GetDate(),
+                        EventType = UserEventType.UserClaimAddedEvent,
+                        MessageId = @event.MessageId,
+                        Payload = message
+                    };
+
+                    await _inboxRepository.AddMessageAsync(inboxMessage);
+                });
+        }
+    }
+}
