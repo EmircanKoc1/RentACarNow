2993bcc94f3d15cf6f23e782486dfd2f6ffeca64
Emircan <emircandv787@gmail.com>
2024-09-11 23:48:42 +0300

    The IBrandEventFactory and BrandEventFactory have been defined.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Implementations/BrandEventFactory.cs
@@ -0,0 +1,28 @@
+﻿using RentACarNow.Common.Events.Brand;
+using RentACarNow.Common.Infrastructure.Factories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Implementations
+{
+    public class BrandEventFactory : IBrandEventFactory
+    {
+        public BrandCreatedEvent CreateBrandCreatedEvent(Guid brandId, string name, string description, DateTime createdDate)
+        {
+            return new BrandCreatedEvent(brandId, name, description, createdDate);
+        }
+
+        public BrandDeletedEvent CreateBrandDeletedEvent(Guid brandId, DateTime deletedDate)
+        {
+            return new BrandDeletedEvent(brandId, deletedDate);
+        }
+
+        public BrandUpdatedEvent CreateBrandUpdatedEvent(Guid brandId, string name, string description, DateTime updatedDate)
+        {
+            return new BrandUpdatedEvent(brandId, name, description, updatedDate);
+        }
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Factories/Interfaces/IBrandEventFactory.cs
@@ -0,0 +1,11 @@
+﻿using RentACarNow.Common.Events.Brand;
+
+namespace RentACarNow.Common.Infrastructure.Factories.Interfaces
+{
+    public interface IBrandEventFactory
+    {
+        BrandCreatedEvent CreateBrandCreatedEvent(Guid brandId, string name, string description, DateTime createdDate);
+        BrandDeletedEvent CreateBrandDeletedEvent(Guid brandId, DateTime deletedDate);
+        BrandUpdatedEvent CreateBrandUpdatedEvent(Guid brandId, string name, string description, DateTime updatedDate);
+    }
+}
