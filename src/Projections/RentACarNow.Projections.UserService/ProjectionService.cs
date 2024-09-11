78ffe4d9e85e202c1fa3502901f8a19d0570deba
Emircan <emircandv787@gmail.com>
2024-09-11 18:41:11 +0300

    The Default Worker has been deleted and ProjectionService has been completed.

---
+++ src/Projections/RentACarNow.Projections.UserService/ProjectionService.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
+using RentACarNow.Common.Enums.RepositoryEnums;
+using RentACarNow.Common.Events.User;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Helpers;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
+using RentACarNow.Common.MongoEntities;
+
+namespace RentACarNow.Projections.UserService
+{
+    public class ProjectionService : BackgroundService
+    {
+        private readonly IUserInboxRepository _userInboxRepository;
+        private readonly ILogger<ProjectionService> _logger;
+        private readonly IMongoUserWriteRepository _userWriteRepository;
+        private readonly IMapper _mapper;
+
+        public ProjectionService(
+            IUserInboxRepository userInboxRepository,
+            ILogger<ProjectionService> logger,
+            IMongoUserWriteRepository userWriteRepository,
+            IMapper mapper)
+        {
+            _userInboxRepository = userInboxRepository;
+            _logger = logger;
+            _userWriteRepository = userWriteRepository;
+            _mapper = mapper;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation($"{nameof(ProjectionService)} execute method has been executed");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var inboxMessages = await _userInboxRepository
+                   .GetMessagesAsync(5, OrderedDirection.None);
+
+                foreach (var inboxMessage in inboxMessages)
+                {
+                    _logger.LogDebug($"{nameof(ProjectionService)} inbox message payload: {inboxMessage.Payload}");
+
+                    var messagePayload = inboxMessage.Payload;
+                    var date = DateHelper.GetDate();
+
+                    switch (inboxMessage.EventType)
+                    {
+                        case UserEventType.UserCreatedEvent:
+                            var userCreatedEvent = messagePayload.Deseralize<UserCreatedEvent>();
+                            await _userWriteRepository.AddAsync(_mapper.Map<User>(userCreatedEvent));
+                            await _userInboxRepository.MarkMessageProccessedAsync(userCreatedEvent.MessageId, date);
+                            break;
+
+                        case UserEventType.UserDeletedEvent:
+                            var userDeletedEvent = messagePayload.Deseralize<UserDeletedEvent>();
+                            await _userWriteRepository.DeleteByIdAsync(userDeletedEvent.Id);
+                            await _userInboxRepository.MarkMessageProccessedAsync(userDeletedEvent.MessageId, date);
+                            break;
+
+                        case UserEventType.UserUpdatedEvent:
+                            var userUpdatedEvent = messagePayload.Deseralize<UserUpdatedEvent>();
+                            await _userWriteRepository.UpdateAsync(_mapper.Map<User>(userUpdatedEvent));
+                            await _userInboxRepository.MarkMessageProccessedAsync(userUpdatedEvent.MessageId, date);
+                            break;
+
+                        case UserEventType.UserClaimAddedEvent:
+                            var userClaimAddedEvent = messagePayload.Deseralize<UserClaimAddedEvent>();
+
+
+                            var addClaim = _mapper.Map<Claim>(userClaimAddedEvent.Claim);
+
+                            await _userWriteRepository.AddUserClaimAsync(userClaimAddedEvent.UserId, addClaim);
+                            await _userInboxRepository.MarkMessageProccessedAsync(userClaimAddedEvent.MessageId, date);
+                            break;
+
+                        case UserEventType.UserClaimDeletedEvent:
+                            var userClaimDeletedEvent = messagePayload.Deseralize<UserClaimDeletedEvent>();
+                            await _userWriteRepository.DeleteUserClaimAsync(userClaimDeletedEvent.UserId, userClaimDeletedEvent.Claim.Id);
+
+
+                            await _userInboxRepository.MarkMessageProccessedAsync(userClaimDeletedEvent.MessageId, date);
+                            break;
+
+                        case UserEventType.UserClaimUpdatedEvent:
+
+                            var userClaimUpdatedEvent = messagePayload.Deseralize<UserClaimUpdatedEvent>();
+                            
+                            var claim = _mapper.Map<Claim>(userClaimUpdatedEvent);
+
+                            await _userWriteRepository.UpdateUserClaimAsync(claim);
+
+                            await _userInboxRepository.MarkMessageProccessedAsync(userClaimUpdatedEvent.MessageId, date);
+                            break;
+
+                        default:
+                            _logger.LogWarning($"{nameof(ProjectionService)} EventType did not match any event");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
