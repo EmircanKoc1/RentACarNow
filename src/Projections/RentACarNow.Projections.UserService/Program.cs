4daf47d84cc5916b978ebbc21b79387fae42e2e4
Emircan <emircandv787@gmail.com>
2024-09-11 18:40:45 +0300

    The Program.cs has been refactored.

--- src/Projections/RentACarNow.Projections.UserService/Program.cs
+++ src/Projections/RentACarNow.Projections.UserService/Program.cs
@@ -1,7 +1,49 @@
+using MongoDB.Driver;
+using RentACarNow.Common.Constants.Databases;
+using RentACarNow.Common.Contexts.InboxContexts.Implementations;
+using RentACarNow.Common.Infrastructure.Extensions;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Write.Mongo;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Write.Mongo;
+using RentACarNow.Projections.UserClaimService.Consumers;
 using RentACarNow.Projections.UserService;
+using RentACarNow.Projections.UserService.Consumers;
+using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<ProjectionService>();
+
+builder.Services.AddHostedService<UserClaimAddedEventConsumer>();
+builder.Services.AddHostedService<UserClaimDeletedEventConsumer>();
+builder.Services.AddHostedService<UserClaimUpdatedEventConsumer>();
+builder.Services.AddHostedService<UserCreatedEventConsumer>();
+builder.Services.AddHostedService<UserDeletedEventConsumer>();
+builder.Services.AddHostedService<UserUpdatedEventConsumer>();
+
+
+
+
+
+builder.Services.AddSingleton<MongoUserInboxContext>(p =>
+{
+    return new MongoUserInboxContext(
+    mongoClient: new MongoClient(MongoDbConstants.CONNECTION_STRING),
+    databaseName: "InboxDB");
+});
+
+builder.Services.AddSingleton<IClaimnboxRepository, ClaimInboxMongoRepository>();
+
+
+builder.Services.AddMongoRentalACarNowDBContext();
+builder.Services.AddSingleton<IMongoUserWriteRepository, MongoUserWriteRepository>();
+
+builder.Services.AddIRabbitMQMessageService(clientName: "UserProjectionService");
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var host = builder.Build();
+
+
+
 host.Run();
