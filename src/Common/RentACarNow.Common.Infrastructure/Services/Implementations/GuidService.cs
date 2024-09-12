d0cd42639e303783affa260405963ab474b30736
Emircan <emircandv787@gmail.com>
2024-09-12 04:54:09 +0300

    The IGuidService and GuidService have been defined.

---
+++ src/Common/RentACarNow.Common.Infrastructure/Services/Implementations/GuidService.cs
@@ -0,0 +1,12 @@
+﻿using RentACarNow.Common.Infrastructure.Services.Interfaces;
+
+namespace RentACarNow.Common.Infrastructure.Services.Implementations
+{
+    public class GuidService : IGuidService
+    {
+        public Guid CreateGuid() => Guid.NewGuid();
+
+        public Guid GetEmptyGuid() => Guid.Empty;
+
+    }
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Services/Interfaces/IGuidService.cs
@@ -0,0 +1,10 @@
+﻿namespace RentACarNow.Common.Infrastructure.Services.Interfaces
+{
+    public interface IGuidService
+    {
+        Guid CreateGuid();
+        Guid GetEmptyGuid();
+
+
+    }
+}
