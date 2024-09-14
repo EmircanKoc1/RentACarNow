12e26555904cb4bae78893eaed84ea4dec1bbea5
Emircan <emircandv787@gmail.com>
2024-09-14 17:43:15 +0300

    CreateRentalCommandRequest has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/CreateRental/CreateRentalCommandRequest.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/CreateRental/CreateRentalCommandRequest.cs
@@ -4,8 +4,8 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.CreateR
 {
     public class CreateRentalCommandRequest : IRequest<CreateRentalCommandResponse>
     {
-        public DateTime? RentalStartedDate { get; set; }
-        public DateTime? RentalEndDate { get; set; }
+        public DateTime RentalStartedDate { get; set; }
+        public DateTime RentalEndDate { get; set; }
 
         public Guid UserId { get; set; }
         public Guid CarId { get; set; }
