36f0fdbebc0336d7ca15793ee48ef237a81df61d
Emircan <emircandv787@gmail.com>
2024-09-14 22:38:05 +0300

    The UpdateRentalCommandRequestHandler has been refactored.

--- src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/UpdateRental/UpdateRentalCommandRequestHandler.cs
+++ src/APIs/WriteAPI/Core/RentACarNow.APIs.WriteAPI.Application/Features/Commands/Rental/UpdateRental/UpdateRentalCommandRequestHandler.cs
@@ -6,11 +6,11 @@ using RentACarNow.APIs.WriteAPI.Application.Features.Commands.Car.UpdateCar;
 using RentACarNow.APIs.WriteAPI.Application.Interfaces.UnitOfWorks;
 using RentACarNow.Common.Entities.OutboxEntities;
 using RentACarNow.Common.Enums.OutboxMessageEventTypeEnums;
-using RentACarNow.Common.Events.Common.Messages;
 using RentACarNow.Common.Events.Rental;
 using RentACarNow.Common.Infrastructure.Extensions;
-using RentACarNow.Common.Infrastructure.Helpers;
+using RentACarNow.Common.Infrastructure.Factories.Interfaces;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Unified;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
 using RentACarNow.Common.Models;
 using System.Net;
 using EfEntity = RentACarNow.APIs.WriteAPI.Domain.Entities.EfCoreEntities;
@@ -25,19 +25,28 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.UpdateR
         private readonly IValidator<UpdateRentalCommandRequest> _validator;
         private readonly ILogger<UpdateRentalCommandRequestHandler> _logger;
         private readonly IMapper _mapper;
+        private readonly IGuidService _guidService;
+        private readonly IDateService _dateService;
+        private readonly IRentalEventFactory _rentalEventFactory;
 
         public UpdateRentalCommandRequestHandler(
             IRentalUnitOfWork rentalUnitOfWork,
             IRentalOutboxRepository rentalOutboxRepository,
             IValidator<UpdateRentalCommandRequest> validator,
             ILogger<UpdateRentalCommandRequestHandler> logger,
-            IMapper mapper)
+            IMapper mapper,
+            IGuidService guidService,
+            IDateService dateService,
+            IRentalEventFactory rentalEventFactory)
         {
             _rentalUnitOfWork = rentalUnitOfWork;
             _rentalOutboxRepository = rentalOutboxRepository;
             _validator = validator;
             _logger = logger;
             _mapper = mapper;
+            _guidService = guidService;
+            _dateService = dateService;
+            _rentalEventFactory = rentalEventFactory;
         }
 
         public async Task<UpdateRentalCommandResponse> Handle(UpdateRentalCommandRequest request, CancellationToken cancellationToken)
@@ -85,15 +94,45 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.UpdateR
 
             }
 
+            var generatedEntityUpdatedDate = _dateService.GetDate();
+            var generatedMessageAddedDate = _dateService.GetDate();
+            var generatedMessageId = _guidService.CreateGuid();
+
 
             var efRentalEntity = _mapper.Map<EfEntity.Rental>(request);
-            efRentalEntity.UpdatedDate = DateHelper.GetDate();
-            
+            efRentalEntity.UpdatedDate = generatedEntityUpdatedDate;
 
-            var rentalUpdatedEvent = _mapper.Map<RentalCreatedEvent>(efRentalEntity);
-            rentalUpdatedEvent.Car = _mapper.Map<CarMessage>(foundedCar);
-            rentalUpdatedEvent.User = _mapper.Map<UserMessage>(foundedUser);
-            rentalUpdatedEvent.UpdatedDate = DateHelper.GetDate();
+            var rentalUpdatedEvent = _rentalEventFactory.CreateRentalUpdatedEvent(
+               rentalId: request.RentalId,
+               rentalStartedDate: request.RentalStartedDate,
+               rentalEndDate: request.RentalEndDate,
+               hourlyRentalPrice: request.HourlyRentalPrice,
+               totalRentalPrice: request.TotalRentalPrice,
+               carId: foundedCar.Id,
+               carName: foundedCar.Name,
+               carModal: foundedCar.Modal,
+               carTitle: foundedCar.Title,
+               carHourlyRentalPrice: foundedCar.HourlyRentalPrice,
+               carKilometer: foundedCar.Kilometer,
+               carDescription: foundedCar.Description,
+               carColor: foundedCar.Color,
+               carPassengerCapacity: foundedCar.PassengerCapacity,
+               carLuggageCapacity: foundedCar.LuggageCapacity,
+               carFuelConsumption: foundedCar.FuelConsumption,
+               carReleaseDate: foundedCar.ReleaseDate,
+               carFuelType: foundedCar.CarFuelType,
+               carTransmissionType: foundedCar.TransmissionType,
+               userId: foundedUser.Id,
+               userName: foundedUser.Name,
+               userSurname: foundedUser.Surname,
+               userAge: foundedUser.Age,
+               userPhoneNumber: foundedUser.PhoneNumber,
+               userEmail: foundedUser.Email,
+               status: request.Status,
+               updatedDate: generatedEntityUpdatedDate).SetMessageId<RentalUpdatedEvent>(generatedMessageId);
+
+
+
 
 
             using var mongoSession = await _rentalOutboxRepository.StartSessionAsync();
@@ -106,8 +145,8 @@ namespace RentACarNow.APIs.WriteAPI.Application.Features.Commands.Rental.UpdateR
 
                 var outboxMessage = new RentalOutboxMessage()
                 {
-                    Id = rentalUpdatedEvent.MessageId,
-                    AddedDate = DateHelper.GetDate(),
+                    Id = generatedMessageId,
+                    AddedDate = generatedMessageAddedDate,
                     EventType = RentalEventType.RentalUpdatedEvent,
                     Payload = rentalUpdatedEvent.Serialize()!
                 };
