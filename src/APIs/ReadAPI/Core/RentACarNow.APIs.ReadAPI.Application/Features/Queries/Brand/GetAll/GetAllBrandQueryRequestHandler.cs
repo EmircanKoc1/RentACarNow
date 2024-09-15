fc8b22b2586b6234b70e20a2def3961a60d1f0ff
Emircan <emircandv787@gmail.com>
2024-09-15 18:56:33 +0300

    The GetAllBrandQueryResponse  and GetAllBrandQueryRequestHandler have been refactored.

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryRequestHandler.cs
@@ -1,32 +1,59 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
+using RentACarNow.Common.Models;
+using System.Net;
 
 namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll
 {
-    public class GetAllBrandQueryRequestHandler : IRequestHandler<GetAllBrandQueryRequest, IEnumerable<GetAllBrandQueryResponse>>
+    public class GetAllBrandQueryRequestHandler : IRequestHandler<GetAllBrandQueryRequest, ResponseWrapper<IEnumerable<GetAllBrandQueryResponse>>>
     {
         private readonly IMongoBrandReadRepository _readRepository;
         private readonly ILogger<GetAllBrandQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
 
-        public GetAllBrandQueryRequestHandler(IMongoBrandReadRepository repository, ILogger<GetAllBrandQueryRequestHandler> logger, IMapper mapper)
+        public GetAllBrandQueryRequestHandler(
+            IMongoBrandReadRepository readRepository,
+            ILogger<GetAllBrandQueryRequestHandler> logger,
+            IMapper mapper)
         {
-            _readRepository = repository;
+            _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<GetAllBrandQueryResponse>> Handle(GetAllBrandQueryRequest request, CancellationToken cancellationToken)
+        public async Task<ResponseWrapper<IEnumerable<GetAllBrandQueryResponse>>> Handle(GetAllBrandQueryRequest request, CancellationToken cancellationToken)
         {
-            var result = await _readRepository.GetAllAsync(
-                paginationParameter: request.PaginationParameter,
-                filter: a => true,
-                orderingParameter: request.OrderingParameter
-            );
+            var paginationParameter = PaginationParameter.CreatePaginationParameter(request.PageNumber, request.PageSize);
 
-            return _mapper.Map<IEnumerable<GetAllBrandQueryResponse>>(result);
+            OrderingParameter? orderingParameter = default;
+
+            if (request.SortingField is null)
+                orderingParameter = OrderingParameter.CreateOrderingParameter(false, false, string.Empty);
+            else
+                orderingParameter = OrderingParameter.CreateOrderingParameter(true, request.IsAscending, request.SortingField);
+
+
+            var brands = await _readRepository.GetAllAsync(
+                paginationParameter: paginationParameter,
+                filter: b => b.DeletedDate == null,
+                orderingParameter: orderingParameter);
+
+            var getAllBrandQueryResponses = _mapper.Map<IEnumerable<GetAllBrandQueryResponse>>(brands);
+
+
+            var paginationInfo = new PaginationInfo
+            {
+                PageItemSize = request.PageSize,
+                PageNumber = request.PageNumber,
+                TotalItemCount = brands.Count()
+            };
+
+
+            return ResponseWrapper<IEnumerable<GetAllBrandQueryResponse>>
+                .Success(getAllBrandQueryResponses, HttpStatusCode.OK, paginationInfo);
         }
     }
 }
--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryResponse.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryResponse.cs
@@ -4,13 +4,12 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll
 {
     public class GetAllBrandQueryResponse
     {
-        public Guid Id { get; set; }
+        public Guid BrandId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public IEnumerable<CarDTO> Cars { get; set; }
-        public DateTime? CreatedDate { get; set; }
-        public DateTime? UpdatedDate { get; set; }
-        public DateTime? DeletedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime DeletedDate { get; set; }
 
 
     }
