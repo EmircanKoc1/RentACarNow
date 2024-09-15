0aca16475ff0132435865afec84d4827e0ddcb5a
Emircan <emircandv787@gmail.com>
2024-09-15 18:55:19 +0300

    The BaseGetAllQueryRequest has been defined.

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Base/BaseGetAllQueryRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Base
+{
+    public class BaseGetAllQueryRequest
+    {
+        public bool IsAscending { get; set; } = true;
+        public string? SortingField { get; set; }
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+
+    }
+}
