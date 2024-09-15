0fcab5a1c09d26139af04df80f8cfb9081770b57
Emircan <emircandv787@gmail.com>
2024-09-15 18:32:14 +0300

    The OrderingParameter has been refactored.

--- src/Common/RentACarNow.Common.Models/OrderingParameter.cs
+++ src/Common/RentACarNow.Common.Models/OrderingParameter.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RentACarNow.Common.Models
+﻿namespace RentACarNow.Common.Models
 {
     public class OrderingParameter
     {
-        public bool Sort { get; set; } 
-        public bool IsAscending { get; set; } = true;
+        public bool Sort { get; set; }
+        public bool IsAscending { get; set; }
         public string SortingField { get; set; }
+
+
+        public OrderingParameter()
+        {
+
+        }
+
+        public OrderingParameter(bool sort, bool isAscending, string sortingField)
+        {
+            Sort = sort;
+            IsAscending = isAscending;
+            SortingField = sortingField;
+        }
+
+        public static OrderingParameter CreateOrderingParameter(bool sort, bool isAscending, string sortingField)
+            => new OrderingParameter(sort, isAscending, sortingField);
+
+
+
     }
 }
