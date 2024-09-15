1906ab67b25fe58b834228fac24b240ec2c3e6a6
Emircan <emircandv787@gmail.com>
2024-09-16 00:13:57 +0300

    The GetByIdBrandQueryRequestHandler has been refactored.

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetById/GetByIdBrandQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetById/GetByIdBrandQueryRequestHandler.cs
@@ -1,19 +1,51 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll;
+using RentACarNow.APIs.ReadAPI.Application.Wrappers;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
+using System.Net;
 
 namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetById
 {
 
-
-    public class GetByIdBrandQueryRequestHandler : IRequestHandler<GetByIdBrandQueryRequest, IEnumerable<GetByIdBrandQueryResponse>>
+    public class GetByIdBrandQueryRequestHandler : IRequestHandler<GetByIdBrandQueryRequest, ResponseWrapper<GetByIdBrandQueryResponse>>
     {
-        public Task<IEnumerable<GetByIdBrandQueryResponse>> Handle(GetByIdBrandQueryRequest request, CancellationToken cancellationToken)
+        private readonly IMongoBrandReadRepository _readRepository;
+        private readonly ILogger<GetAllBrandQueryRequestHandler> _logger;
+        private readonly IMapper _mapper;
+
+        public GetByIdBrandQueryRequestHandler(
+            IMongoBrandReadRepository readRepository,
+            ILogger<GetAllBrandQueryRequestHandler> logger,
+            IMapper mapper)
         {
-            throw new NotImplementedException();
+            _readRepository = readRepository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseWrapper<GetByIdBrandQueryResponse>> Handle(GetByIdBrandQueryRequest request, CancellationToken cancellationToken)
+        {
+            var entity = await _readRepository.GetByIdAsync(request.BrandId);
+
+            if (entity is null)
+                return ResponseWrapper<GetByIdBrandQueryResponse>.Success(
+                  data: default,
+                  statusCode: HttpStatusCode.NotFound,
+                  paginationInfo: null);
+
+            var responseData = _mapper.Map<GetByIdBrandQueryResponse>(entity);
+
+            return ResponseWrapper<GetByIdBrandQueryResponse>
+                .Success(
+                data: responseData,
+                statusCode: HttpStatusCode.OK,
+                paginationInfo: null);
+
+
+
+
         }
     }
 
