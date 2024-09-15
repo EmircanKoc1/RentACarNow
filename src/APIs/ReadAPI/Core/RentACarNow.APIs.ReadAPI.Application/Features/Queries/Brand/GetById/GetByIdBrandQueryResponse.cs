1630eec7cd8d4ae749d06b9c060ed5e57b9642ba
Emircan <emircandv787@gmail.com>
2024-09-15 18:55:56 +0300

    The GetByIdBrandQueryResponse has been refactored.

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetById/GetByIdBrandQueryResponse.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetById/GetByIdBrandQueryResponse.cs
@@ -5,10 +5,9 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetById
     public class GetByIdBrandQueryResponse
     {
 
-        public Guid Id { get; set; }
+        public Guid BrandId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public IEnumerable<CarDTO> Cars { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
         public DateTime? DeletedDate { get; set; }
