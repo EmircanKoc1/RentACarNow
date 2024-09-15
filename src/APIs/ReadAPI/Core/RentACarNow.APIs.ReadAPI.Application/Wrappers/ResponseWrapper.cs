ba2d2cd7ec6db0ba88a2eba09f4072a9869b8c6e
Emircan <emircandv787@gmail.com>
2024-09-15 18:12:39 +0300

    The ResponseWrapper  and PaginationInfo classes have been added

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Wrappers/ResponseWrapper.cs
@@ -0,0 +1,44 @@
+﻿using RentACarNow.Common.Models;
+using System.Net;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Wrappers
+{
+    public class ResponseWrapper<TData>
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public TData? Data { get; set; }
+        public PaginationInfo? PaginationInfo { get; set; }
+        public IEnumerable<string>? ErrorMessages { get; set; }
+
+
+
+        public static ResponseWrapper<TData> Success(TData data, HttpStatusCode statusCode, PaginationInfo? paginationInfo)
+        {
+            return new ResponseWrapper<TData>
+            {
+                Data = data,
+                PaginationInfo = paginationInfo,
+                HttpStatusCode = statusCode,
+                ErrorMessages = default
+            };
+        }
+
+        public static ResponseWrapper<TData> Fail(HttpStatusCode statusCode, IEnumerable<string> errors)
+        {
+            return new ResponseWrapper<TData>
+            {
+                Data = default,
+                PaginationInfo = default,
+                ErrorMessages = errors,
+                HttpStatusCode = statusCode,
+            };
+        }
+
+
+
+    }
+
+
+
+
+}
---
+++ src/Common/RentACarNow.Common.Models/PaginationInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace RentACarNow.Common.Models
+{
+    public class PaginationInfo
+    {
+        public int PageNumber { get; set; }
+        public int PageItemSize { get; set; }
+        public int TotalItemCount { get; set; }
+        public int TotalPageCount => (int)Math.Ceiling((double)TotalItemCount / PageItemSize);
+
+    }
+}
