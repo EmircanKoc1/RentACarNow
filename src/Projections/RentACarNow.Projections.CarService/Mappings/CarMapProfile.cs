b23e185aa28e48b7299c2f1ec0f9e3b2c5bf13ed
Emircan <emircandv787@gmail.com>
2024-09-16 18:59:08 +0300

    The CarService CarMapProfile has been refactored.

--- src/Projections/RentACarNow.Projections.CarService/Mappings/CarMapProfile.cs
+++ src/Projections/RentACarNow.Projections.CarService/Mappings/CarMapProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using RentACarNow.Common.Events.Car;
+using RentACarNow.Common.Events.Common.Messages;
 using RentACarNow.Common.MongoEntities;
 using System;
 using System.Collections.Generic;
@@ -13,10 +14,17 @@ namespace RentACarNow.Projections.CarService.Mappings
     {
         public CarMapProfile()
         {
-            CreateMap<CarCreatedEvent, Car>();
-            CreateMap<CarDeletedEvent, Car>();
-            CreateMap<CarUpdatedEvent, Car>();
-            
+            CreateMap<CarCreatedEvent, Car>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(cce => cce.CarId));
+            CreateMap<CarDeletedEvent, Car>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(cde => cde.CarId));
+
+            CreateMap<CarUpdatedEvent, Car>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(cue => cue.CarId));
+
+            CreateMap<BrandMessage,Brand>()
+                .ForMember(dest => dest.Id, src => src.MapFrom(bce => bce.BrandId));
+
         }
     }
 }
