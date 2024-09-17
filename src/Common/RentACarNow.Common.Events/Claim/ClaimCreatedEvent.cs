5095c4369b8c0b9985746023b77dc776bb7d5cc3
Emircan <emircandv787@gmail.com>
2024-09-17 19:29:42 +0300

    The ClaimCreatedEvent has been refactored.

--- src/Common/RentACarNow.Common.Events/Claim/ClaimCreatedEvent.cs
+++ src/Common/RentACarNow.Common.Events/Claim/ClaimCreatedEvent.cs
@@ -4,19 +4,19 @@ namespace RentACarNow.Common.Events.Claim
 {
     public class ClaimCreatedEvent : BaseEvent
     {
-        public ClaimCreatedEvent(Guid claimId, string key, string value, DateTime claimCreatedDate)
+        public ClaimCreatedEvent(Guid claimId, string key, string value, DateTime createdDate)
         {
             ClaimId = claimId;
             Key = key;
             Value = value;
-            ClaimCreatedDate = claimCreatedDate;
+            CreatedDate = createdDate;
         }
 
         public Guid ClaimId { get; set; }
         public string Key { get; set; }
         public string Value { get; set; }
 
-        public DateTime ClaimCreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
 
     }
 }
