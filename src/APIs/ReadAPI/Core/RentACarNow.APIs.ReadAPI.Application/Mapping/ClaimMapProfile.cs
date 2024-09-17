b118fbc7835ee21e4282a8615f0858c3723e75b0
Emircan <emircandv787@gmail.com>
2024-09-17 03:13:42 +0300

    The ClaimMapProfile has been refactored.

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Mapping/ClaimMapProfile.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Mapping/ClaimMapProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using RentACarNow.APIs.ReadAPI.Application.DTOs;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetById;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetAll;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetById;
 using RentACarNow.Common.MongoEntities;
 using System;
 using System.Collections.Generic;
@@ -16,6 +19,13 @@ namespace RentACarNow.APIs.ReadAPI.Application.Mapping
         {
             CreateMap<ClaimDTO, Claim>()
                 .ReverseMap();
+
+            CreateMap<Claim, GetAllClaimQueryResponse>()
+                .ForMember(dest => dest.ClaimId, src => src.MapFrom(c => c.Id));
+
+            CreateMap<Claim, GetByIdClaimQueryResponse>()
+               .ForMember(dest => dest.ClaimId, src => src.MapFrom(c => c.Id));
+
 
 
         }
