fc05bc78f6620305f5eba56fffc8fb8c0c7539c7
Emircan <emircandv787@gmail.com>
2024-09-18 19:07:55 +0300

    The UserMapProfile has been added .

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Mapping/UserMapProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using RentACarNow.APIs.ReadAPI.Application.DTOs;
+using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Events.Common.Messages;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Mapping
+{
+    public class UserMapProfile : Profile
+    {
+        public UserMapProfile()
+        {
+            CreateMap<UserMessage, User>()
+                .ForMember(u => u.Id, src => src.MapFrom(um => um.UserId));
+
+            CreateMap<User, UserDTO>()
+                .ForMember(u => u.Id, src => src.MapFrom(ud => ud.Id));
+
+        }
+    }
+}
