fd7b5e891115ff7e60a933f45b5e1b70ae241ad8
Emircan <emircandv787@gmail.com>
2024-09-18 20:58:29 +0300

    The IMongoUserReadRepository and MongoUserRepository have been added .

---
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Implementations/Read/Mongo/MongoUserReadRepository.cs
@@ -0,0 +1,15 @@
+﻿using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Infrastructure.Repositories.Implementations.Base;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
+using RentACarNow.Common.MongoContexts.Implementations;
+
+namespace RentACarNow.Common.Infrastructure.Repositories.Implementations.Read.Mongo
+{
+    public class MongoUserReadRepository : MongoBaseReadRepository<User>, IMongoUserReadRepository
+    {
+        public MongoUserReadRepository(MongoRentalACarNowDbContext context) : base(context)
+        {
+        }
+    }
+
+}
---
+++ src/Common/RentACarNow.Common.Infrastructure/Repositories/Interfaces/Read/Mongo/IMongoUserReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Base;
+
+namespace RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo
+{
+    public interface IMongoUserReadRepository : IMongoReadRepository<User>
+    {
+    }
+
+
+}
