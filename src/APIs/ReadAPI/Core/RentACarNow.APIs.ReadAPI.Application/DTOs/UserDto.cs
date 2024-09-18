d2f714dce8de229b01ef2684cae4a95c4fa1384b
Emircan <emircandv787@gmail.com>
2024-09-18 17:30:23 +0300

    The UserDTO has been added .

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/DTOs/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACarNow.APIs.ReadAPI.Application.DTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public decimal WalletBalance { get; set; }
+
+    }
+}
