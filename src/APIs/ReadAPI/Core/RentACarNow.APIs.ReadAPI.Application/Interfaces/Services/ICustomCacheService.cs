518bec0e55ec24aebed8dcc0f2593dd40ced16a1
Emircan <emircandv787@gmail.com>
2024-09-19 21:55:49 +0300

    The ICustomCacheService has been added .

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Interfaces/Services/ICustomCacheService.cs
@@ -0,0 +1,21 @@
+﻿using RentACarNow.Common.Models;
+using System.Linq.Expressions;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Interfaces.Services
+{
+    public interface ICustomCacheService<TEntity>
+    {
+        TEntity? GetEntity(Guid id);
+
+        void SetEntity(Guid id, TEntity entity, TimeSpan cacheDeleteTime);
+        void SetDbEntityCount(long entityCount);
+        long GetDbEntityCount();
+        long GetCacheEntityCount();
+        void SetEntities(IEnumerable<TEntity> entities, TimeSpan cacheDeleteTime);
+
+        IEnumerable<TEntity?> GetEntities(PaginationParameter paginationParameter, Expression<Func<TEntity, bool>> filter);
+
+
+    }
+
+}
