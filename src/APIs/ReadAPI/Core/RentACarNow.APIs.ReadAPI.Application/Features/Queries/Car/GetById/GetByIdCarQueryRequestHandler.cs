4e2a5c4fe7a0d397534a3d6a29a212b8afe1c9ec
Emircan <emircandv787@gmail.com>
2024-09-19 23:05:08 +0300

    Added caching functionality to the GetByIdCarQueryHandler class

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Car/GetById/GetByIdCarQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Car/GetById/GetByIdCarQueryRequestHandler.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll;
-using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetById;
+using RentACarNow.APIs.ReadAPI.Application.Interfaces.Services;
 using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
 using System.Net;
@@ -15,42 +14,47 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Car.GetById
         private readonly ILogger<GetByIdCarQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
         private readonly ResponseBuilder<GetByIdCarQueryResponse> _responseBuilder;
-
+        private readonly ICustomCarCacheService _cacheService;
         public GetByIdCarQueryRequestHandler(
             IMongoCarReadRepository readRepository,
             ILogger<GetByIdCarQueryRequestHandler> logger,
             IMapper mapper,
-            ResponseBuilder<GetByIdCarQueryResponse> responseBuilder)
+            ResponseBuilder<GetByIdCarQueryResponse> responseBuilder,
+            ICustomCarCacheService cacheService)
         {
             _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
             _responseBuilder = responseBuilder;
+            _cacheService = cacheService;
         }
 
         public async Task<ResponseWrapper<GetByIdCarQueryResponse>> Handle(GetByIdCarQueryRequest request, CancellationToken cancellationToken)
         {
-            var entity = await _readRepository.GetByIdAsync(request.CarId);
-
-            //var responseBuilder = ResponseWrapper<GetByIdBrandQueryResponse>
-            //    .Builder();
-
-            var responseBuilder = _responseBuilder;
+            var entity = _cacheService.GetEntity(request.CarId);
 
             if (entity is null)
             {
-                return responseBuilder
-                    .SetHttpStatusCode(HttpStatusCode.NotFound)
-                    .Build();
+                entity = await _readRepository.GetByIdAsync(request.CarId);
+
+                if (entity is null)
+                    return _responseBuilder
+                        .SetHttpStatusCode(HttpStatusCode.NotFound)
+                        .Build();
+
             }
+
+            _cacheService.SetEntity(entity.Id, entity, TimeSpan.FromMinutes(1));
 
             var responseData = _mapper.Map<GetByIdCarQueryResponse>(entity);
 
 
-            return responseBuilder
+            return _responseBuilder
                 .SetData(responseData)
                 .SetHttpStatusCode(HttpStatusCode.OK)
                 .Build();
+
+
 
 
         }
