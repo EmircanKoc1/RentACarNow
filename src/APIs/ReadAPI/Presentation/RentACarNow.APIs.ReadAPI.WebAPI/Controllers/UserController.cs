5da4a8abc2d42dc42014f721ca15749b26148a02
Emircan <emircandv787@gmail.com>
2024-09-19 04:47:28 +0300

    The UserController has been added .

---
+++ src/APIs/ReadAPI/Presentation/RentACarNow.APIs.ReadAPI.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.User.GetAll;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.User.GetById;
+
+namespace RentACarNow.APIs.ReadAPI.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]/[action]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IMediator mediator, ILogger<UserController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] GetAllUserQueryRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById([FromQuery] GetByIdUserQueryRequest request)
+        {
+            return Ok(await _mediator.Send(request));
+        }
+
+
+    }
+
+}
