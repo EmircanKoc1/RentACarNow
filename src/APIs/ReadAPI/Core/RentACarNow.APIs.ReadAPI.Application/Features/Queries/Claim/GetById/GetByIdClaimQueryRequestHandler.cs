3bef3f462f74df2df76def0680abaa9d70a105f7
Emircan <emircandv787@gmail.com>
2024-09-19 23:19:20 +0300

    Added caching functionality to the GetByIdClaimQueryHandler class

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Claim/GetById/GetByIdClaimQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Claim/GetById/GetByIdClaimQueryRequestHandler.cs
@@ -1,11 +1,10 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Car.GetById;
-using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetAll;
+using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetById;
+using RentACarNow.APIs.ReadAPI.Application.Interfaces.Services;
 using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
-using SharpCompress.Common;
 using System.Net;
 
 namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetById
@@ -16,41 +15,47 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetById
         private readonly ILogger<GetByIdClaimQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
         private readonly ResponseBuilder<GetByIdClaimQueryResponse> _responseBuilder;
-
+        private readonly ICustomClaimCacheService _cacheService;
         public GetByIdClaimQueryRequestHandler(
-            IMongoClaimReadRepository readRepository, 
-            ILogger<GetByIdClaimQueryRequestHandler> logger, 
-            IMapper mapper, 
-            ResponseBuilder<GetByIdClaimQueryResponse> responseBuilder)
+            IMongoClaimReadRepository readRepository,
+            ILogger<GetByIdClaimQueryRequestHandler> logger,
+            IMapper mapper,
+            ResponseBuilder<GetByIdClaimQueryResponse> responseBuilder,
+            ICustomClaimCacheService cacheService)
         {
             _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
             _responseBuilder = responseBuilder;
+            _cacheService = cacheService;
         }
 
         public async Task<ResponseWrapper<GetByIdClaimQueryResponse>> Handle(GetByIdClaimQueryRequest request, CancellationToken cancellationToken)
         {
 
-            var responseBuilder = _responseBuilder;
-            
-            var entity = await _readRepository.GetByIdAsync(request.ClaimId);
-
+            var entity = _cacheService.GetEntity(request.ClaimId);
 
             if (entity is null)
             {
-                return responseBuilder
-                    .SetHttpStatusCode(HttpStatusCode.NotFound)
-                    .Build();
+                entity = await _readRepository.GetByIdAsync(request.ClaimId);
+
+                if (entity is null)
+                    return _responseBuilder
+                        .SetHttpStatusCode(HttpStatusCode.NotFound)
+                        .Build();
+
             }
+
+            _cacheService.SetEntity(entity.Id, entity, TimeSpan.FromMinutes(1));
 
             var responseData = _mapper.Map<GetByIdClaimQueryResponse>(entity);
 
 
-            return responseBuilder
+            return _responseBuilder
                 .SetData(responseData)
                 .SetHttpStatusCode(HttpStatusCode.OK)
                 .Build();
+
 
 
 
