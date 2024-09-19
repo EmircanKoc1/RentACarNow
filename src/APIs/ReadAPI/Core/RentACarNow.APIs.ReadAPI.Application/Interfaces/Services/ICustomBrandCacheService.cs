73134a229e6b4fe972efbc76c0781fa6a2539ba4
Emircan <emircandv787@gmail.com>
2024-09-19 21:57:16 +0300

    The ICustomBrandCacheService ,ICustomCarCacheService , ICustomClaimCacheService , ICustomRentalCacheService , and ICustomUserCacheService have been added .

---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Interfaces/Services/ICustomBrandCacheService.cs
@@ -0,0 +1,10 @@
+﻿using RentACarNow.Common.MongoEntities;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Interfaces.Services
+{
+    public interface ICustomBrandCacheService : ICustomCacheService<Brand>
+    {
+
+    }
+
+}
---
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Interfaces/Services/ICustomUserCacheService.cs
@@ -0,0 +1,10 @@
+﻿using RentACarNow.Common.Entities.MongoEntities;
+
+namespace RentACarNow.APIs.ReadAPI.Application.Interfaces.Services
+{
+    public interface ICustomUserCacheService : ICustomCacheService<User>
+    {
+
+    }
+
+}
