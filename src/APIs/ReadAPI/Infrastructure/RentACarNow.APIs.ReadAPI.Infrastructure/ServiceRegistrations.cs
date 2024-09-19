de4c08c58176b09edb5f414acbf432bab9597a72
Emircan <emircandv787@gmail.com>
2024-09-19 21:58:00 +0300

    The services have been added to the DI container.

---
+++ src/APIs/ReadAPI/Infrastructure/RentACarNow.APIs.ReadAPI.Infrastructure/ServiceRegistrations.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RentACarNow.Common.Entities.MongoEntities;
+using RentACarNow.Common.Infrastructure.Services.Implementations;
+using RentACarNow.Common.Infrastructure.Services.Interfaces;
+using RentACarNow.Common.MongoEntities;
+using System.Collections.Concurrent;
+using ST = System.Timers;
+namespace RentACarNow.APIs.ReadAPI.Infrastructure
+{
+    public static class ServiceRegistrations
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+
+            services.AddSingleton<ConcurrentDictionary<Guid, User>>();
+            services.AddSingleton<ConcurrentDictionary<Guid, Brand>>();
+            services.AddSingleton<ConcurrentDictionary<Guid, Claim>>();
+            services.AddSingleton<ConcurrentDictionary<Guid, Car>>();
+            services.AddSingleton<ConcurrentDictionary<Guid, Rental>>();
+
+
+            services.AddTransient<ST.Timer>();
+
+            services.AddTransient<ConcurrentDictionary<DateTime, Guid>>();
+
+            services.AddSingleton<IDateService, UtcNowDateService>();
+
+
+            return services;
+
+        }
+
+    }
+}
