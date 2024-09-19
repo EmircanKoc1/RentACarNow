bc210c643db53578077d04050e5ee1e7f2f38466
Emircan <emircandv787@gmail.com>
2024-09-20 02:50:36 +0300

    Added caching functionality to the GetByIdUserQueryHandler class

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/User/GetById/GetByIdUserQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/User/GetById/GetByIdUserQueryRequestHandler.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using RentACarNow.APIs.ReadAPI.Application.Features.Queries.Claim.GetById;
+using RentACarNow.APIs.ReadAPI.Application.Interfaces.Services;
 using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
 using System.Net;
@@ -14,40 +14,47 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.User.GetById
         private readonly ILogger<GetByIdUserQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
         private readonly ResponseBuilder<GetByIdUserQueryResponse> _responseBuilder;
-
+        private readonly ICustomUserCacheService _cacheService;
         public GetByIdUserQueryRequestHandler(
-            IMongoUserReadRepository readRepository, 
-            ILogger<GetByIdUserQueryRequestHandler> logger, 
-            IMapper mapper, 
-            ResponseBuilder<GetByIdUserQueryResponse> responseBuilder)
+            IMongoUserReadRepository readRepository,
+            ILogger<GetByIdUserQueryRequestHandler> logger,
+            IMapper mapper,
+            ResponseBuilder<GetByIdUserQueryResponse> responseBuilder,
+            ICustomUserCacheService cacheService)
         {
             _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
             _responseBuilder = responseBuilder;
+            _cacheService = cacheService;
         }
 
         public async Task<ResponseWrapper<GetByIdUserQueryResponse>> Handle(GetByIdUserQueryRequest request, CancellationToken cancellationToken)
         {
-            var responseBuilder = _responseBuilder;
 
-            var entity = await _readRepository.GetByIdAsync(request.UserId);
-
+            var entity = _cacheService.GetEntity(request.UserId);
 
             if (entity is null)
             {
-                return responseBuilder
-                    .SetHttpStatusCode(HttpStatusCode.NotFound)
-                    .Build();
+                entity = await _readRepository.GetByIdAsync(request.UserId);
+
+                if (entity is null)
+                    return _responseBuilder
+                        .SetHttpStatusCode(HttpStatusCode.NotFound)
+                        .Build();
+
             }
+
+            _cacheService.SetEntity(entity.Id, entity, TimeSpan.FromMinutes(1));
 
             var responseData = _mapper.Map<GetByIdUserQueryResponse>(entity);
 
 
-            return responseBuilder
+            return _responseBuilder
                 .SetData(responseData)
                 .SetHttpStatusCode(HttpStatusCode.OK)
                 .Build();
+
 
         }
     }
