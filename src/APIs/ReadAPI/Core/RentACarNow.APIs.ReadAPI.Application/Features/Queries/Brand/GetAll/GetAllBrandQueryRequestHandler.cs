454afdc23f10c8a948c33027e70fac7567558b92
Emircan <emircandv787@gmail.com>
2024-09-20 04:47:23 +0300

    Cache support has been added to the GetAllBrandQueryRequestHandler class

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Brand/GetAll/GetAllBrandQueryRequestHandler.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.ReadAPI.Application.Interfaces.Services;
 using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
 using RentACarNow.Common.Models;
 using System.Net;
+using ME = RentACarNow.Common.MongoEntities;
 
 namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll
 {
@@ -13,37 +15,52 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Brand.GetAll
         private readonly IMongoBrandReadRepository _readRepository;
         private readonly ILogger<GetAllBrandQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
-
+        private readonly ICustomBrandCacheService _cacheService;
         public GetAllBrandQueryRequestHandler(
             IMongoBrandReadRepository readRepository,
             ILogger<GetAllBrandQueryRequestHandler> logger,
-            IMapper mapper)
+            IMapper mapper,
+            ICustomBrandCacheService brandCacheService,
+            ICustomBrandCacheService cacheService)
         {
             _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
+            _cacheService = cacheService;
         }
 
         public async Task<ResponseWrapper<IEnumerable<GetAllBrandQueryResponse>>> Handle(GetAllBrandQueryRequest request, CancellationToken cancellationToken)
         {
+
             var paginationParameter = PaginationParameter.CreatePaginationParameter(request.PageNumber, request.PageSize);
 
             OrderingParameter? orderingParameter = default;
 
+            IEnumerable<ME.Brand?> entities = default;
+
+
             if (request.SortingField is null)
+            {
                 orderingParameter = OrderingParameter.CreateOrderingParameter(false, false, string.Empty);
+            }
             else
                 orderingParameter = OrderingParameter.CreateOrderingParameter(true, request.IsAscending, request.SortingField);
 
 
-            var brands = await _readRepository.GetAllAsync(
+            entities = await _readRepository.GetAllAsync(
                 paginationParameter: paginationParameter,
                 filter: b => b.DeletedDate == null,
                 orderingParameter: orderingParameter);
 
-            var totalItemCount = await _readRepository.CountAsync();
+            long totalItemCount = 0;
 
-            var getAllBrandQueryResponses = _mapper.Map<IEnumerable<GetAllBrandQueryResponse>>(brands);
+            if (_cacheService.GetDbEntityCount() <= 0)
+            {
+                totalItemCount = await _readRepository.CountAsync();
+                _cacheService.SetDbEntityCount(totalItemCount);
+            }
+
+            var getAllBrandQueryResponses = _mapper.Map<IEnumerable<GetAllBrandQueryResponse>>(entities);
 
 
             var paginationInfo = new PaginationInfo
