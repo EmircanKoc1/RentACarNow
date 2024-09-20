43fe281bd70e863e7840204e330c967e32f219e7
Emircan <emircandv787@gmail.com>
2024-09-20 04:58:34 +0300

    Cache support has been added to the GetAllCarQueryRequestHandler class

--- src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Car/GetAll/GetAllCarQueryRequestHandler.cs
+++ src/APIs/ReadAPI/Core/RentACarNow.APIs.ReadAPI.Application/Features/Queries/Car/GetAll/GetAllCarQueryRequestHandler.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using RentACarNow.APIs.ReadAPI.Application.Interfaces.Services;
 using RentACarNow.APIs.ReadAPI.Application.Wrappers;
 using RentACarNow.Common.Infrastructure.Repositories.Interfaces.Read.Mongo;
 using RentACarNow.Common.Models;
@@ -14,17 +15,19 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Car.GetAll
         private readonly ILogger<GetAllCarQueryRequestHandler> _logger;
         private readonly IMapper _mapper;
         private readonly ResponseBuilder<IEnumerable<GetAllCarQueryResponse>> _responseBuilder;
-
+        private readonly ICustomCarCacheService _cacheService;
         public GetAllCarQueryRequestHandler(
             IMongoCarReadRepository readRepository,
             ILogger<GetAllCarQueryRequestHandler> logger,
             IMapper mapper,
-            ResponseBuilder<IEnumerable<GetAllCarQueryResponse>> responseBuilder)
+            ResponseBuilder<IEnumerable<GetAllCarQueryResponse>> responseBuilder,
+            ICustomCarCacheService cacheService)
         {
             _readRepository = readRepository;
             _logger = logger;
             _mapper = mapper;
             _responseBuilder = responseBuilder;
+            _cacheService = cacheService;
         }
 
         public async Task<ResponseWrapper<IEnumerable<GetAllCarQueryResponse>>> Handle(GetAllCarQueryRequest request, CancellationToken cancellationToken)
@@ -44,7 +47,13 @@ namespace RentACarNow.APIs.ReadAPI.Application.Features.Queries.Car.GetAll
                 filter: c => c.DeletedDate == null,
                 orderingParameter: orderingParameter);
 
-            var totalItemCount = await _readRepository.CountAsync();
+            long totalItemCount = 0;
+
+            if (_cacheService.GetDbEntityCount() <= 0)
+            {
+                totalItemCount = await _readRepository.CountAsync();
+                _cacheService.SetDbEntityCount(totalItemCount);
+            }
 
             var getAllBrandQueryResponses = _mapper.Map<IEnumerable<GetAllCarQueryResponse>>(cars);
 
